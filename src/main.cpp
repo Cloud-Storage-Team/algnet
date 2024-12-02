4b7ac718b0d48d11879af34053d6d26352ba1ef6
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-02 18:49:32 +0300

    Remove unnecessary files

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "NetworkSimulator.hpp"
-
-int main() {
-    std::vector<std::uint32_t> distances_to_receiver_ns = {500, 1000, 2000};
-
-    NetworkSimulator ns(distances_to_receiver_ns);
-    ns.Run();
-}
