28072bb5e57f47f718190ab619b40596b16c2a00
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-04 01:35:24 +0300

    Add id and storage, improve comments

--- include/NetworkDevice.hpp
+++ include/NetworkDevice.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "Packet.hpp"
+
 #include <vector>
 #include <memory>
 #include <cstdint>
+#include <unordered_map>
 
 enum class DeviceType: std::uint8_t {
     Switch,
@@ -12,18 +15,31 @@ enum class DeviceType: std::uint8_t {
 
 class NetworkDevice {
 public:
-    explicit NetworkDevice(DeviceType type);
+    explicit NetworkDevice(DeviceType type, std::uint32_t id);
     virtual ~NetworkDevice() = default;
 
     /**
-     * @brief Vector of pointers to adjacent network devices
+     * @brief Vector of pointers to adjacent network devices.
      *
-     * Usage of std::weak_ptr is necessary to avoid cyclic links
+     * @details Usage of std::weak_ptr is necessary to avoid cyclic links.
      */
     std::vector<std::weak_ptr<NetworkDevice>> adjacent_devices;
 
     /**
-     * @brief Network device type
+     * @brief Network device type.
      */
     DeviceType type;
+
+    /**
+     * @brief Network device ID.
+     */
+    std::uint32_t id;
+
+    /**
+     * @brief Network device storage.
+     *
+     * @details Device stores packets from adjacent device in corresponding vector. \n
+     * Key: adjacent device ID. Value: vector with packets.
+     */
+    std::unordered_map<std::uint32_t, std::vector<Packet>> storage;
 };
