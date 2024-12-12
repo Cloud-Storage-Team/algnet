922d4b9b0cfc6170e5a9f0e5112b7f9bfafca277
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-12 13:42:59 +0300

    Update Flow

--- include/Flow.hpp
+++ include/Flow.hpp
@@ -1,25 +1,67 @@
 #pragma once
 
-#include "Packet.hpp"
 #include "NetworkDevice.hpp"
 
 #include <cstdint>
 #include <memory>
+#include <queue>
+#include <tuple>
 
 /**
  * @brief Network flow between the source and destination nodes
  */
 class Flow {
 public:
-    explicit Flow(const NetworkDevice& src, const NetworkDevice& dst);
+    explicit Flow(const std::vector<std::shared_ptr<NetworkDevice>>& path, const std::vector<std::uint32_t>& distances_ns);
 
     /**
-     * @brief Pointer to flow's source node
+     * @brief Send packets from server sender.
      */
-    std::shared_ptr<NetworkDevice> source_node;
+    void Send();
 
     /**
-     * @brief Pointer to flow's destination node
+     * @brief Find adjacent devices on the path
+     *
+     * @param id -- ID of current device
+     *
+     * @returns tuple {PrevID, PrevDist, NextID, NextDist}
+     * @returns PrevID -- ID of previous (closer to sender) device on the path
+     * @returns PrevDist -- distance in nanoseconds to device with ID = PrevID
+     * @returns NextID -- ID of next (closer to receiver) device on the path
+     * @returns NextDist -- distance in nanoseconds to device with ID = NextID
      */
-    std::shared_ptr<NetworkDevice> destination_node;
+    std::tuple<std::uint32_t, std::uint64_t, std::uint32_t, std::uint64_t> FindAdjDevicesInPath(std::uint32_t id);
+
+    /**
+     * @brief Names for FindAdjDevicesInPath(id) return values
+     */
+    static const std::uint8_t PrevID = 0;
+    static const std::uint8_t PrevDist = 1;
+    static const std::uint8_t NextID = 2;
+    static const std::uint8_t NextDist = 3;
+
+    /**
+     * @brief ID of the flow
+     *
+     * Usage: event routing in the network
+     */
+    std::uint32_t id;
+
+    /**
+     * @brief Vector of network devices forming the flow path
+     *
+     * @details First element is the source node, last element is the destination node
+     */
+    std::vector<std::shared_ptr<NetworkDevice>> path{};
+
+    /**
+     * @brief Vector of distances in nanoseconds between nodes on the path
+     */
+    std::vector<std::uint32_t> distances_ns{};
+
+    /**
+     * @brief New flow ID = last given + 1
+     */
+    inline static std::uint32_t last_given_flow_id = 0;
+
 };
--- src/Flow.cpp
+++ src/Flow.cpp
@@ -1,5 +1,35 @@
 #include "Flow.hpp"
 
-Flow::Flow(const NetworkDevice& src, const NetworkDevice& dst):
-    source_node(std::make_shared<NetworkDevice>(src)),
-    destination_node(std::make_shared<NetworkDevice>(dst)) { }
+#include <stdexcept>
+
+Flow::Flow(const std::vector<std::shared_ptr<NetworkDevice>>& path, const std::vector<std::uint32_t>& distances_ns) {
+    if (path.size() < 2) {
+        throw std::runtime_error("Error: Incorrect flow path.");
+    }
+    if (distances_ns.size() != path.size() - 1) {
+        throw std::runtime_error("Error: Incorrect flow distances.");
+    }
+    this->path = path;
+    this->distances_ns = distances_ns;
+    id = Flow::last_given_flow_id++;
+}
+
+void Flow::Send() {
+    std::shared_ptr<NetworkDevice> server = path.at(0);
+    server->Send(id);
+}
+
+std::tuple<std::uint32_t, std::uint64_t, std::uint32_t, std::uint64_t> Flow::FindAdjDevicesInPath(std::uint32_t id) {
+    for (std::uint32_t i = 1; i < path.size() - 1; ++i) {
+        if (path[i]->id == id) {
+            return {path[i - 1]->id, distances_ns[i - 1], path[i + 1]->id, distances_ns[i]};
+        }
+    }
+    if (path[0]->id == id) {
+        return {path[1]->id, distances_ns.front(), path[1]->id, distances_ns.front()};
+    }
+    if (path[path.size() - 1]->id == id) {
+        return {path[path.size() - 2]->id, distances_ns.back(), path[path.size() - 2]->id, distances_ns.back()};
+    }
+    return {0, 0, 0, 0};
+}
