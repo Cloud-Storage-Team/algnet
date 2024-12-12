ad84176c215f86ea0b51de759584d45dee75d408
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-12 13:46:59 +0300

    Update Flow::Send()

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -1,17 +1,8 @@
-#include "Flow.hpp"
-#include "Server.hpp"
+#include "NetworkDevice.hpp"
 
-Flow::Flow(const std::vector<std::uint32_t>& distances) : destination_node(0) {
-    for (std::uint32_t distance : distances) {
-        source_nodes.emplace_back(++last_given_id, distance);
-    }
-}
+#include <memory>
 
 void Flow::Send() {
-    for (ServerSender& s: source_nodes) {
-        for (std::uint64_t i = 0; i < s.GetCWNDSize(); ++i) {
-            packets.emplace(s.GetID(), s.GetCurrentTime(), s.GetCurrentTime() + s.GetDistance());
-            s.IncreaseCurrentTime(s.GetDistance());
-        }
-    }
+    std::shared_ptr<NetworkDevice> server = path.at(0);
+    server->Send(id);
 }
