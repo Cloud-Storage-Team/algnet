4c11404d6efa639384f18348e99d110ef1285ac2
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-12 13:47:54 +0300

    Update Packet

--- include/Packet.hpp
+++ include/Packet.hpp
@@ -4,26 +4,14 @@
 
 class Packet {
 public:
-    Packet(std::uint64_t sender_id, std::uint64_t sending_time_ns, std::uint64_t delivery_time_ns);
+    explicit Packet(std::uint64_t sending_time_ns);
     Packet() = default;
     ~Packet() = default;
-
-    bool operator<(const Packet& other) const;
-
-    /**
-     * @brief ID of the sending network device
-     */
-    std::uint32_t sender_id;
 
     /**
      * @brief Packet sending time in nanoseconds
      */
     std::uint64_t sending_time_ns;
-
-    /**
-     * @brief Packet delivery time in nanoseconds
-     */
-    std::uint64_t delivery_time_ns;
 
     /**
      * @brief Number of successfully delivered bytes
