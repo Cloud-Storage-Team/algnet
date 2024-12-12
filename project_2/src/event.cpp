0a7a56d239e23ecf2e75ba893fb90174ec5c78fd
Artyom Peshkov <artemyipeshkov@gmail.com>
2024-12-12 17:20:01 +0300

    remove

--- project_2/src/event.cpp
+++
@@ -1,9 +0,0 @@
-#include "event.hpp"
-#include <string>
-
-Event::Event(std::uint64_t event_time, std::uint32_t priority): event_time(event_time), priority(priority) {}
-
-bool Event::operator<(const Event& other) const {
-    return (this->event_time > other.event_time) || 
-           (this->event_time == other.event_time && this->priority > other.priority);
-}
