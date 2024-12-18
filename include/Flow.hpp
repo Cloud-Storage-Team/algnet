ed140c287e4b532106208c1e1ace91b06046741d
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-18 04:04:19 +0300

    Update Flow

--- include/Flow.hpp
+++ include/Flow.hpp
@@ -10,11 +10,12 @@ class Flow {
 public:
     explicit Flow(const std::vector<std::shared_ptr<NetworkDevice>>& path, const std::vector<std::uint32_t>& distances_ns);
 
-    void Send(double delta_time_ns);
+    void Send();
 
     std::uint32_t id;
     std::vector<std::shared_ptr<NetworkDevice>> path{};
     std::vector<std::uint32_t> distances_ns{};
 
+    double interval_ns;
     inline static std::uint32_t last_given_flow_id = 0;
 };
--- src/Flow.cpp
+++ src/Flow.cpp
@@ -14,19 +14,3 @@ Flow::Flow(const std::vector<std::shared_ptr<NetworkDevice>>& path, const std::v
     this->distances_ns = distances_ns;
     id = Flow::last_given_flow_id++;
 }
-
-void Flow::Send(double delta_time_ns) {
-    std::shared_ptr<NetworkDevice> sender = path.front();
-    std::shared_ptr<NetworkDevice> receiver = path.back();
-
-    double current_time_ns = 0;
-    while (current_time_ns < 1e9) {
-        Packet p(sender->id, receiver->id, 0, false);
-        NetworkSimulator::Schedule(current_time_ns, [&]() {
-            sender->Enqueue(p);
-            double delay_ns = 1.0;
-            NetworkSimulator::Schedule(delay_ns, [&](){ sender->ProcessPacket(); });
-        });
-        current_time_ns += delta_time_ns;
-    }
-}
