db5f4e9d8e4cb64c8c4be920ffe6da60058f5dbc
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-18 04:06:39 +0300

    Update NetworkDevice: add queue, ProcessPacket

--- src/NetworkDevice.cpp
+++ src/NetworkDevice.cpp
@@ -1,4 +1,19 @@
 #include "NetworkDevice.hpp"
 
-NetworkDevice::NetworkDevice(DeviceType type):
-    type(type) { }
+NetworkDevice::NetworkDevice(DeviceType type, double processing_delay_ns):
+    type(type),
+    processing_delay_ns(processing_delay_ns) { }
+
+void NetworkDevice::Enqueue(Packet p) {
+    buffer.push(p);
+}
+
+Packet NetworkDevice::Dequeue() {
+    Packet result = buffer.front();
+    buffer.pop();
+    return result;
+}
+
+bool NetworkDevice::Empty() {
+    return buffer.empty();
+}
