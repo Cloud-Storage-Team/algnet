7cb9f7d8ae54f243be53114c70d41bcb12c02495
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-20 18:15:41 +0300

    Update capture list in event handler

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -5,9 +5,9 @@
 void Flow::Send() {
     std::shared_ptr<NetworkDevice> sender = path.front();
     std::shared_ptr<NetworkDevice> receiver = path.back();
-
     Packet p(sender->id, receiver->id, 0, false);
-    NetworkSimulator::Schedule(interval_ns, [&]() {
+
+    NetworkSimulator::Schedule(packet_generation_interval_ns, [this, sender, p]() {
         sender->ProcessPacket(p);
         Send();
     });
