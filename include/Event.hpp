a04480bbe49764f9c00c654013b835c75bd2df1e
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-20 17:47:50 +0300

    Update Event

--- include/Event.hpp
+++ include/Event.hpp
@@ -4,39 +4,17 @@
 
 #include <memory>
 #include <cstdint>
+#include <functional>
 
 class Event {
 public:
-    explicit Event(std::shared_ptr<Packet> packet, std::uint32_t flow_id, std::uint32_t receiver_id,
-                   std::uint64_t delivery_time_ns, bool is_ACK);
+    Event(std::uint64_t time, const std::function<void()>& handler);
 
-    /**
-     * @brief Pointer to the packet
-     */
-    std::shared_ptr<Packet> packet;
+    std::uint64_t GetTime() const;
+    void Execute() const;
 
-    /**
-     * @brief ID of network device -- receiver of the event
-     */
-    std::uint32_t receiver_id;
-
-    /**
-     * @brief ID of the corresponding flow
-     */
-    std::uint32_t flow_id;
-
-    /**
-     * @brief Packet delivery time in nanoseconds
-     */
-    std::uint64_t delivery_time_ns;
-
-    /**
-     * @brief Acknowledgement flag
-     *
-     * @details True: ACK
-     * @details False: Packet
-     */
-    bool is_ACK;
+    std::uint64_t execution_time;
+    std::function<void()> handler;
 
     bool operator<(const Event& other) const;
 };
