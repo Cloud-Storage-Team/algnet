61aea1fbcd9b6f10eb3312a6d91348917ed92118
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-24 03:53:52 +0300

    Update Flow::Send method

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -1,31 +1,12 @@
-#include <memory>
-#include <stdexcept>
-
 #include "Flow.hpp"
 #include "NetworkSimulator.hpp"
 
-Flow::Flow(const std::vector<std::shared_ptr<NetworkDevice>>& path,
-           const std::vector<std::uint32_t>& distances_ns,
-           std::uint64_t packet_generation_interval_ns):
-        packet_generation_interval_ns(packet_generation_interval_ns) {
-    if (path.size() < 2) {
-        throw std::runtime_error("Error: Incorrect flow path.");
-    }
-    if (distances_ns.size() != path.size() - 1) {
-        throw std::runtime_error("Error: Incorrect flow distances.");
-    }
-    this->path = path;
-    this->distances_ns = distances_ns;
-    id = Flow::last_given_flow_id++;
-}
-
 void Flow::Send() {
-    std::shared_ptr<NetworkDevice> sender = path.front();
-    std::shared_ptr<NetworkDevice> receiver = path.back();
-    Packet p(sender->id, receiver->id, 0, false);
+    Packet p(m_sender->id, m_receiver->id, 0, false);
 
-    NetworkSimulator::Schedule(packet_generation_interval_ns, [this, sender, p]() {
-        sender->ProcessPacket(p);
+    NetworkSimulator::Schedule(m_packet_generation_interval_ns, [this, p]() {
+        m_sender->ProcessPacket(p);
         Send();
     });
+
 }
