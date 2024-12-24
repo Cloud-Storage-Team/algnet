f954fe7f9839b4c5a6e17178293dacea5d48abbe
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-24 03:48:33 +0300

    Update NetworkSimulator -- routing table, link

--- include/NetworkSimulator.hpp
+++ include/NetworkSimulator.hpp
@@ -5,6 +5,7 @@
 #include "Switch.hpp"
 #include "Sender.hpp"
 #include "Receiver.hpp"
+#include "Link.hpp"
 
 #include <cstdint>
 #include <vector>
@@ -23,28 +24,20 @@ public:
     void Run() const;
     void StopAt(std::uint64_t time_ns);
     static void AddDevice(std::shared_ptr<NetworkDevice> device);
-    static void EmplaceFlow(const std::vector<std::shared_ptr<NetworkDevice>>& flow_path,
-                            const std::vector<std::uint32_t>& flow_distances_ns,
-                            std::uint64_t flow_packet_generation_interval_ns);
+    static void AddLink(std::shared_ptr<Link> link, std::shared_ptr<Flow> flow);
+    static void AddFlow(std::shared_ptr<Flow> flow);
     static void Schedule(std::uint64_t delay, const std::function<void(void)>& handler);
     static std::uint64_t Now();
 
-    static std::uint32_t GetDistanceNs(std::uint32_t src_id, std::uint32_t dst_id);
-    static std::uint64_t GetLinkLastProcessTime(std::uint32_t src_id, std::uint32_t dst_id);
-    static void UpdateLinkLastProcessTime(std::uint32_t src_id, std::uint32_t dst_id, std::uint64_t value);
-
     std::uint64_t stop_time_ns = 1'000'000'000;
     static inline std::unique_ptr<EventScheduler> event_scheduler;
-    static inline std::unordered_map<std::uint32_t, std::shared_ptr<NetworkDevice>> forward_routing_table;
-    static inline std::unordered_map<std::uint32_t, std::shared_ptr<NetworkDevice>> backward_routing_table;
+    static inline std::map<std::pair<std::uint32_t, std::uint32_t>, std::shared_ptr<Link>> routing_table;
     static inline std::unordered_map<std::uint32_t, std::shared_ptr<NetworkDevice>> device_by_id;
 
     static inline std::uint64_t current_time_ns = 0;
     static const std::uint32_t packet_size_bytes = 1024;
     static const std::uint64_t bandwidth_bytes = 6'250'000'000;
-    static inline std::vector<std::unique_ptr<Flow>> flows;
-    static inline std::map<std::pair<std::uint32_t, std::uint32_t>, std::uint64_t> link_last_process_time_ns;
-    static inline std::map<std::pair<std::uint32_t, std::uint32_t>, std::uint32_t> distances_ns;
+    static inline std::vector<std::shared_ptr<Flow>> flows;
 };
 
 namespace Time {
--- src/NetworkSimulator.cpp
+++ src/NetworkSimulator.cpp
@@ -4,39 +4,15 @@
 void NetworkSimulator::Run() const {
     event_scheduler = std::make_unique<EventScheduler>();
 
-    /* Fill link time map and distances map*/
-    for (std::unique_ptr<Flow>& flow: flows) {
-        for (std::uint32_t i = 0; i < flow->path.size() - 1; ++i) {
-            /* Forward link */
-            link_last_process_time_ns[{flow->path[i]->id, flow->path[i + 1]->id}] = 0;
-            /* Backward link */
-            link_last_process_time_ns[{flow->path[i + 1]->id, flow->path[i]->id}] = 0;
-
-            distances_ns[{std::min(flow->path[i]->id, flow->path[i + 1]->id),
-                          std::max(flow->path[i]->id, flow->path[i + 1]->id)}] = flow->distances_ns[i];
-        }
-    }
-
-    /* Create routing tables */
-    for (std::unique_ptr<Flow>& flow: flows) {
-        for (std::uint32_t i = 0; i < flow->path.size() - 1; ++i) {
-            if (forward_routing_table.count(flow->path[i]->id) == 0) {
-                forward_routing_table[flow->path[i]->id] = device_by_id[flow->path[i + 1]->id];
-            }
-            if (backward_routing_table.count(flow->path[i + 1]->id) == 0) {
-                backward_routing_table[flow->path[i + 1]->id] = device_by_id[flow->path[i]->id];
-            }
-        }
-    }
     /* Start packet sending from each sender */
-    for (std::unique_ptr<Flow>& flow: flows) {
+    for (std::shared_ptr<Flow>& flow: flows) {
         flow->Send();
     }
 
     /* Process events */
     while (!event_scheduler->Empty() && event_scheduler->PeekNextEventTime() < stop_time_ns) {
         Event e = event_scheduler->PopNextEvent();
-        current_time_ns = e.GetTime();
+        current_time_ns = e.execution_time;
         e.Execute();
     }
 }
@@ -49,10 +25,14 @@ void NetworkSimulator::AddDevice(std::shared_ptr<NetworkDevice> device) {
     device_by_id[device->id] = device;
 }
 
-void NetworkSimulator::EmplaceFlow(const std::vector<std::shared_ptr<NetworkDevice>>& flow_path,
-                                   const std::vector<std::uint32_t>& flow_distances_ns,
-                                   std::uint64_t flow_packet_generation_interval_ns) {
-    flows.emplace_back(new Flow(flow_path, flow_distances_ns, flow_packet_generation_interval_ns));
+void NetworkSimulator::AddLink(std::shared_ptr<Link> link, std::shared_ptr<Flow> flow) {
+    // pair <current_device, receiver_id> --> link to next device
+    routing_table[{link->source->id, flow->receiver->id}] = link;
+    routing_table[{link->destination->id, flow->sender->id}] = link;
+}
+
+void NetworkSimulator::AddFlow(std::shared_ptr<Flow> flow) {
+    flows.push_back(flow);
 }
 
 void NetworkSimulator::Schedule(std::uint64_t delay, const std::function<void()>& handler) {
@@ -61,18 +41,6 @@ void NetworkSimulator::Schedule(std::uint64_t delay, const std::function<void()>
 
 std::uint64_t NetworkSimulator::Now() {
     return current_time_ns;
-}
-
-std::uint32_t NetworkSimulator::GetDistanceNs(std::uint32_t src_id, std::uint32_t dst_id) {
-    return NetworkSimulator::distances_ns[{std::min(src_id, dst_id), std::max(src_id, dst_id)}];
-}
-
-std::uint64_t NetworkSimulator::GetLinkLastProcessTime(std::uint32_t src_id, std::uint32_t dst_id) {
-    return NetworkSimulator::link_last_process_time_ns[{src_id, dst_id}];
-}
-
-void NetworkSimulator::UpdateLinkLastProcessTime(std::uint32_t src_id, std::uint32_t dst_id, std::uint64_t value) {
-    NetworkSimulator::link_last_process_time_ns[{src_id, dst_id}] = value;
 }
 
 std::uint64_t Time::Seconds(std::uint64_t time_s) {
