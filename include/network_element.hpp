5bc397e011ae3c2bfef8a515c9387bc5d4dcb3ca
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-01-24 14:14:03 +0300

    fixes

--- include/network_element.hpp
+++ include/network_element.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "link.hpp"
 #include "packet.hpp"
 #include "event.hpp"
 
@@ -7,56 +8,17 @@
 #include <queue>
 #include <memory>
 
-class RoutingPacket;
+class Link;
 
-/**
- * @brief Abstract network element (link, switch, host, etc.)
- * 
- */
 class NetworkElement {
+protected:
+    std::unordered_map<std::uint64_t, std::shared_ptr<Link>> routing_table{};
 public:
     NetworkElement() = default;
-    
-    /**
-     * @brief get next element in route
-     * 
-     */
-    virtual std::shared_ptr<NetworkElement> GetNextElement(std::uint64_t destination_id) = 0;
+    NetworkElement(const std::unordered_map<std::uint64_t, std::shared_ptr<Link>>& routing_table);
 
-    /**
-     * @brief Processes the obtained packet (basically adds processing time to packet's sending time)
-     * 
-     */
-    virtual void ReceivePacket(std::uint64_t current_time_ns, PacketHeader& packet, std::priority_queue<std::shared_ptr<Event>, std::vector<std::shared_ptr<Event>>, EventComparator>& all_events) = 0;
-    virtual ~NetworkElement() {}
-};
+    void AddNewRoute(std::uint64_t destination_id, std::shared_ptr<Link>& link);
 
-/**
- * @brief Abstract network element that has routing table (switch, host, etc.)
- * 
- */
-class RoutingNetworkElement: public NetworkElement {
-protected:
-    std::unordered_map<std::uint64_t, std::shared_ptr<NetworkElement>> routing_table{};
-public:
-    RoutingNetworkElement() = default;
-    /**
-     * @brief Construct a new Routing Network Element
-     * 
-     */
-    RoutingNetworkElement(const std::unordered_map<std::uint64_t, std::shared_ptr<NetworkElement>>& routing_table);
-
-    /**
-     * @brief Creates new record in routing table
-     * 
-     */
-    void AddNewRoute(std::uint64_t destination_id, std::shared_ptr<NetworkElement>& link);
-
-
-    /**
-     * @brief get next element in route
-     * 
-     */
-    std::shared_ptr<NetworkElement> GetNextElement(std::uint64_t destination_id) override final;
+    virtual std::shared_ptr<Link> GetNextElement(std::uint64_t destination_id);
     virtual void ReceivePacket(std::uint64_t current_time_ns, PacketHeader& packet, std::priority_queue<std::shared_ptr<Event>, std::vector<std::shared_ptr<Event>>, EventComparator>& all_events) = 0;
 };
--- include/server.hpp
+++ include/server.hpp
@@ -9,11 +9,7 @@
 #include <vector>
 #include "event.hpp"
 
-/**
- * @brief Host abstraction
- * 
- */
-class ServerBase: public RoutingNetworkElement {
+class ServerBase: public NetworkElement {
 protected:
     //! Host id (should be given ONLY by Simulator::GenerateNewID)
     std::uint64_t id;
@@ -25,16 +21,8 @@ public:
         return id;
     }
 
-    /**
-     * @brief Generates new packets from host
-     * 
-     */
     virtual std::uint64_t SendPackets(std::uint64_t current_time_ns, std::priority_queue<std::shared_ptr<Event>, std::vector<std::shared_ptr<Event>>, EventComparator>& all_events) = 0;
     
-    /**
-     * @brief Returns some statistics, calculated by host
-     * 
-     */
     virtual std::string ToString() const = 0;
     virtual void ReceivePacket(std::uint64_t current_time_ns, PacketHeader& packet, std::priority_queue<std::shared_ptr<Event>, std::vector<std::shared_ptr<Event>>, EventComparator>& all_events) override = 0;
     virtual ~ServerBase() {}
