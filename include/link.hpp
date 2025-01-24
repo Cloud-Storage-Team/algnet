524020c17bb60b0e101aeba993988b02afffdb23
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-01-24 14:22:03 +0300

    added link

---
+++ include/link.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "network_element.hpp"
+
+#include <vector>
+#include <cstdint>
+#include <queue>
+#include <memory>
+#include <unordered_map>
+
+class NetworkElement;
+class Event;
+struct EventComparator;
+
+class Link {
+protected:
+    std::shared_ptr<NetworkElement> source;
+    std::shared_ptr<NetworkElement> destination;
+    // In bits per ns
+    std::uint64_t speed;
+    // Stores time when the last packet was pushed through
+    std::uint64_t last_process_time_ns;
+    
+public:
+    Link() = default;
+
+    Link(std::shared_ptr<NetworkElement>& source, std::shared_ptr<NetworkElement>& destination, std::uint64_t speed, std::uint64_t last_process_time_ns = 0);
+    std::shared_ptr<NetworkElement> GetNextElement(std::uint64_t destination_id = 0);
+    void ReceivePacket(std::uint64_t current_time_ns, PacketHeader& packet, std::priority_queue<std::shared_ptr<Event>, std::vector<std::shared_ptr<Event>>, EventComparator>& all_events);
+    virtual ~Link() {}
+};
+
+struct Connection {
+    std::shared_ptr<Link> direct_link;
+    std::shared_ptr<Link> reversed_link;
+
+    Connection(std::shared_ptr<Link>&& direct_link, std::shared_ptr<Link>&& reversed_link): 
+        direct_link(std::move(direct_link)), 
+        reversed_link(std::move(reversed_link)) {}
+};
