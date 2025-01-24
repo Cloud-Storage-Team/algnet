05e415d53cc8cf5f90a63629678cc29252ab351c
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-01-24 14:00:36 +0300

    fixes

--- include/packet.hpp
+++ include/packet.hpp
@@ -3,16 +3,6 @@
 #include <cstdint>
 #include <ostream>
 
-#ifdef DEBUG
-#define PRINT_PACKET(pack) std::cout << pack << std::endl
-#else
-#define PRINT_PACKET(pack)
-#endif
-
-/**
- * @brief Network packet abstraction
- * 
- */
 class PacketHeader {
 public:
     std::uint64_t source_id;
@@ -24,31 +14,27 @@ public:
     std::uint64_t RTT;
 
     // 0 bit - is credit packet
-    // 1 bit - is ACK
     // 2 bit - initializing connection or not
     std::uint8_t flags = 0;
 
     PacketHeader() = default;
-    /**
-     * @brief Construct a new Packet Header object
-     * 
-     */
-    PacketHeader(std::uint64_t source_id, std::uint64_t destination_id, std::uint64_t sending_time, std::uint32_t packet_index, std::uint32_t size);
+    PacketHeader(std::uint64_t source_id, std::uint64_t destination_id, std::uint64_t sending_time, std::uint32_t packet_index, std::uint64_t RTT, std::uint32_t size);
 
     std::uint8_t GetFlag(std::uint8_t bit) const;
     void SetFlag(std::uint8_t bit, std::uint8_t value);
 
-#ifdef DEBUG
+    bool operator<(const PacketHeader& other) const;
+
     friend std::ostream& operator<<(std::ostream& outs, const PacketHeader & packet) {
         outs << "PacketHeader("
              << "Source ID: " << packet.source_id << ", "
              << "Destination ID: " << packet.destination_id << ", "
              << "Sending Time: " << packet.sending_time << ", "
              << "Packet Index: " << packet.packet_index << ", "
+             << "RTT: " << packet.RTT << ", "
              << "Size: " << packet.size << ", "
              << "Flags: " << static_cast<int>(packet.flags)
              << ")";
         return outs;
     }
-#endif
 };
--- src/packet.cpp
+++ src/packet.cpp
@@ -1,11 +1,12 @@
 #include "packet.hpp"
 #include <string>
 
-PacketHeader::PacketHeader(std::uint64_t source_id, std::uint64_t destination_id, std::uint64_t sending_time, std::uint32_t packet_index, std::uint32_t size):
+PacketHeader::PacketHeader(std::uint64_t source_id, std::uint64_t destination_id, std::uint64_t sending_time, std::uint32_t packet_index, std::uint64_t RTT, std::uint32_t size):
     source_id(source_id),
     destination_id(destination_id),
     sending_time(sending_time),
     packet_index(packet_index),
+    RTT(RTT),
     size(size) {}
 
 
@@ -20,4 +21,9 @@ void PacketHeader::SetFlag(std::uint8_t bit, std::uint8_t value) {
     } else if (value == 1) {
         flags = flags | ((std::uint8_t)1 << bit);
     }
+}
+
+bool PacketHeader::operator<(const PacketHeader& other) const {
+    return (this->sending_time > other.sending_time) || 
+           (this->sending_time == other.sending_time && this->destination_id > other.destination_id);
 }
