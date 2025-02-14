ec72495e1a21075cdbe2383e35217e3167a82ff5
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-02-14 13:34:04 +0300

    fix

--- source/device.hpp
+++ source/device.hpp
@@ -1,39 +1,39 @@
-    #pragma once
+#pragma once
 
-    #include <set>
-    #include <unordered_map>
+#include <set>
+#include <unordered_map>
 
-    #include "flow.hpp"
-    #include "link.hpp"
-    #include "packet.hpp"
+#include "flow.hpp"
+#include "link.hpp"
+#include "packet.hpp"
 
-    namespace sim {
+namespace sim {
 
-    enum DeviceType { SWITCH, SENDER, RECEIVER };
+enum DeviceType { SWITCH, SENDER, RECEIVER };
 
-    class Device {
-    public:
-        Device(DeviceType a_type);
-        virtual ~Device();
-        virtual void process();
+class Device {
+public:
+    Device(DeviceType a_type);
+    virtual ~Device();
+    virtual void process();
 
-        void add_inlink(Link* link);
-        void add_outlink(Link* link, Device* dest);
+    void add_inlink(Link* link);
+    void add_outlink(Link* link, Device* dest);
 
-        // TODO: add update_routing_table method
-        // TODO: add get_neighbors method
+    // TODO: add update_routing_table method
+    // TODO: add get_neighbors method
 
-    private:
-        DeviceType m_type;
+private:
+    DeviceType m_type;
 
-        // Ordered set as we need to iterate over the ingress buffers
-        std::set<Link*> m_inlinks;
+    // Ordered set as we need to iterate over the ingress buffers
+    std::set<Link*> m_inlinks;
 
-        // Iterator for the next ingress to process
-        std::set<Link*>::iterator m_next_inlink;
+    // Iterator for the next ingress to process
+    std::set<Link*>::iterator m_next_inlink;
 
-        // A routing table: maps destination node to a specific link
-        std::unordered_map<Device*, Link*> m_outlinks;
-    };
+    // A routing table: maps destination node to a specific link
+    std::unordered_map<Device*, Link*> m_outlinks;
+};
 
-    }  // namespace sim
+}  // namespace sim
