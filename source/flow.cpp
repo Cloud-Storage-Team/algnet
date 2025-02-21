b22c024fb3d5c7d76f485278a6e92c691af8704e
BogruAKVD <bnmmnb456654@inbox.ru>
2025-02-21 12:20:05 +0300

    review fixes + clang-format

--- source/flow.cpp
+++ source/flow.cpp
@@ -1,70 +1,68 @@
 #include "flow.hpp"
-#include "event.hpp"
-#include "scheduler.hpp"
-#include "packet.hpp"
-#include "sender.hpp"
+
 #include <iostream>
+
+#include "event.hpp"
+#include "packet.hpp"
+#include "scheduler.hpp"
+#include "sender.hpp"
 
 namespace sim {
 
-    Flow::Flow(Device* src, size_t packet_size, size_t delay_between_packets, size_t total_packets)
-        : m_src(src), 
-          m_packet_size(packet_size), 
-          m_delay_between_packets(delay_between_packets), 
-          m_total_packets(total_packets),
-          m_packets_sent(0),
-          m_cwnd(1), 
-          m_ssthresh(128), 
-          m_linear_coeficient(1),
-          m_packets_in_flight(0), 
-          m_packets_acked(0),
-          m_last_send_time(0) {}
+Flow::Flow(Device* src, size_t packet_size, size_t delay_between_packets,
+           size_t total_packets, size_t congestion_delay)
+    : m_src(src),
+      m_packet_size(packet_size),
+      m_delay_between_packets(delay_between_packets),
+      m_total_packets(total_packets),
+      m_packets_sent(0),
+      m_cwnd(1),
+      m_ssthresh(128),
+      m_linear_coeficient(1),
+      m_packets_in_flight(0),
+      m_packets_acked(0),
+      m_last_send_time(0),
+      m_delay(congestion_delay) {}
 
-    void Flow::start(size_t time){
-        schedule_packet_generation(time);
-    };
+void Flow::start(int time) { schedule_packet_generation(time); };
 
-    bool Flow::try_to_generate(size_t current_time) { 
-        if (m_packets_in_flight < m_cwnd && m_packets_sent<m_total_packets) { //maybe good to stop generating events if all packets are sent
-            schedule_packet_generation(std::max(m_last_send_time + m_delay_between_packets, current_time));
-            return true;
-        }
-        return false;
+bool Flow::try_to_generate(size_t current_time) {
+    if (m_packets_in_flight < m_cwnd &&
+        m_packets_sent < m_total_packets) {  // maybe good to stop generating
+                                             // events if all packets are sent
+        schedule_packet_generation(
+            std::max(m_last_send_time + m_delay_between_packets, current_time));
+        return true;
     }
+    return false;
+}
 
-    void Flow::update(size_t delay){
-        if (delay < 100){
-            ack();
-        } else {
-            trigger_congestion();
-        }
-    }
-
-    void Flow::ack() {
+void Flow::update(size_t delay) {
+    if (delay < m_delay) {  // ack
         m_packets_in_flight--;
-        if (m_cwnd < m_ssthresh){
+        if (m_cwnd < m_ssthresh) {
             m_cwnd++;
         } else {
             m_packets_acked++;
-            if (m_linear_coeficient * m_packets_acked >=  m_cwnd) {
-                m_cwnd ++; 
+            if (m_packets_acked >= m_cwnd / m_linear_coeficient) {
+                m_cwnd++;
                 m_packets_acked = 0;
             }
         }
-    }
-
-    void Flow::trigger_congestion() {
-        m_ssthresh = m_cwnd / 2; 
+    } else {  // trigger_congestion
+        m_ssthresh = m_cwnd / 2;
         m_cwnd = 1;
+        m_packets_in_flight = 0;
     }
+}
 
-    void Flow::schedule_packet_generation(int time) {
-        Generate generate_event(this, m_packet_size);
-        generate_event.time = time;
-        Scheduler::get_instance().add(generate_event);
-        ++m_packets_in_flight;
-        ++m_packets_sent;
-        m_last_send_time = time;
-    }
+void Flow::schedule_packet_generation(int time) {
+    Generate generate_event(this, m_packet_size);
+    generate_event.time = time;
+    Scheduler::get_instance().add(generate_event);
+    m_packets_in_flight++;
+    m_packets_sent++;
+    m_last_send_time = time;
+}
 
 }  // namespace sim
--- source/flow.hpp
+++ source/flow.hpp
@@ -2,45 +2,57 @@
 
 namespace sim {
 
-
 class Packet;
 
 class Flow {
 public:
-    Flow(Device* src, size_t packet_size, size_t delay_between_packets, size_t total_packets);
+    Flow(Device* src, size_t packet_size, size_t delay_between_packets,
+         size_t total_packets, size_t congestion_delay);
 
     void start(int time);
 
     // Try to generate a new packet if the internal state allows to do so.
     // by placing it into the flow buffer of the source node.
     // Schedule the next generation event.
-    bool try_to_generate();
+    bool try_to_generate(size_t current_time);
 
-    void update(Packet packet);
+    /*
+    Slow Start:
+        The algorithm increases the m_cwnd until it reaches a threshold
+        (m_ssthresh). Initially, m_cwnd starts at 1 and increases by 1 for each
+        ACK received.
+    */
 
-    Device *m_src;
+    /*Congestion Avoidance:
+        Once m_cwnd reaches m_ssthresh, the
+        algorithm enters a congestion avoidance phase where it increases m_cwnd
+        by 1 for every m_cwnd / m_linear_coeficient packets acknowledged. */
+
+    /*Congestion Detection and Reaction:
+        The algorithm checks if the delay is less than a
+        specified threshold (m_delay). If it is, an ACK is processed; otherwise,
+        congestion is triggered. Upon detecting congestion, the algorithm halves
+        the m_ssthresh and resets m_cwnd to 1
+    */
+    void update(size_t delay);
+
+    Device* m_src;
 
 private:
     void schedule_packet_generation(int time);
 
-    // Update the internal state according to some congestion control algorithm
-    // Call on sender-only if ACK packet was received 
-    void ack();
-
-    // Call on sender-only in case of congestions
-    void trigger_congestion();
-    
     size_t m_packet_size;
     size_t m_delay_between_packets;
     size_t m_total_packets;
     size_t m_packets_sent;
     size_t m_last_send_time;
 
-    size_t m_cwnd;                      // Congestion window
-    size_t m_ssthresh;                  // Slow start threshold
+    size_t m_cwnd;      // Congestion window
+    size_t m_ssthresh;  // Slow start threshold
     size_t m_linear_coeficient;
     size_t m_packets_in_flight;
     size_t m_packets_acked;
+    size_t m_delay;  // delay for update
 };
 
 }  // namespace sim
