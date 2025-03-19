67fa98ca4fe1a45c0e5d5fe28af72c9f7b5f7b91
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-03-20 00:02:43 +0300

    fix checks

--- source/device.cpp
+++ source/device.cpp
@@ -10,10 +10,6 @@
 namespace sim {
 
 void RoutingModule::add_inlink(std::shared_ptr<ILink> link) {
-    if (link == nullptr) {
-        spdlog::warn("Link is nullptr");
-        return;
-    }
     m_inlinks.insert(link);
     m_next_inlink = m_inlinks.begin();
 }
@@ -24,7 +20,6 @@ void RoutingModule::update_routing_table(std::shared_ptr<IRoutingDevice> dest,
         spdlog::warn("Unexpected nullptr link");
         return;
     }
-
     auto link_dest = link->get_to();    
 
     // TODO: discuss storing weak_ptrs instead of shared
@@ -34,13 +29,8 @@ void RoutingModule::update_routing_table(std::shared_ptr<IRoutingDevice> dest,
 
 std::vector<std::shared_ptr<IRoutingDevice>> RoutingModule::get_neighbours()
     const {
-    std::unordered_set<std::shared_ptr<IRoutingDevice>> outlinks;
-    for (auto device_link : m_routing_table) {
-        outlinks.emplace((device_link.second)->get_to());
-    }
-
     std::vector<std::shared_ptr<IRoutingDevice>> neighbours{};
-    neighbours.insert(neighbours.begin(), outlinks.begin(), outlinks.end());
+    neighbours.insert(neighbours.begin(), m_neighbours.begin(), m_neighbours.end());
     return neighbours;
 }
 
