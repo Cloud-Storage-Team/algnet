7460e0b3f13fe6b83edb69eb2d7a56064bc74203
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-03-21 14:31:40 +0300

    actualize reseiver

--- source/receiver.cpp
+++ source/receiver.cpp
@@ -1,5 +1,6 @@
 #include "receiver.hpp"
 
+#include <spdlog/spdlog.h>
 #include <iostream>
 
 #include "event.hpp"
@@ -11,8 +12,13 @@
 namespace sim {
 
 void Receiver::add_inlink(std::shared_ptr<ILink> link) {
+    if (link == nullptr) {
+        spdlog::warn("Passed link is null");
+        return;
+    }
+
     if (this != link->get_to().get()) {
-        // Add log
+        spdlog::warn("Link destination device is incorrect (expected current device)");
         return;
     }
     m_router->add_inlink(link);
@@ -21,7 +27,7 @@ void Receiver::add_inlink(std::shared_ptr<ILink> link) {
 void Receiver::update_routing_table(std::shared_ptr<IRoutingDevice> dest,
                                      std::shared_ptr<ILink> link) {
     if (this != link->get_from().get()) {
-        // Add log
+        spdlog::warn("Link source device is incorrect (expected current device)");
         return;
     }
     m_router->update_routing_table(dest, link);
@@ -35,11 +41,16 @@ void Receiver::process() {
     std::shared_ptr<ILink> current_inlink = m_router->next_inlink();
     std::uint32_t total_processing_time = 0;
 
-    // TODO: process current_inlink = nullptr
+    if (current_inlink == nullptr) {
+        spdlog::warn("No available inlinks for device");
+        return;
+    }
 
     std::optional<Packet> opt_data_packet = current_inlink->get_packet();
-
-    // TODO: process no packet
+    if (!opt_data_packet.has_value()) {
+        spdlog::warn("No available packets from inlink for device");
+        return;
+    }
 
     Packet data_packet = opt_data_packet.value();
 
@@ -47,11 +58,10 @@ void Receiver::process() {
     // total_processing_time += processing_time;
 
 
-    // TODO: send ack
-    // Packet ack = {PacketType::ACK, 1, data_packet.flow};
-    // std::shared_ptr<ILink> link_to_src = m_router->get  m_routing_table[data_packet.flow->get_source()];
-    // // Not sure if we want to send ack before processing or after it
-    // link_to_src->schedule_arrival(ack);
+    Packet ack = {PacketType::ACK, 1, data_packet.flow};
+    std::shared_ptr<ILink> link_to_src = m_router->get_link_to_destination(data_packet.flow->get_source());
+    // Not sure if we want to send ack before processing or after it
+    link_to_src->schedule_arrival(ack);
 
     // return total_processing_time;
 }
