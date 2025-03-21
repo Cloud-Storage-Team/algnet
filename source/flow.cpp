b443e876197527168650442b2f9b05dcd17f7995
BogruAKVD <bnmmnb456654@inbox.ru>
2025-03-21 14:36:41 +0300

    create IFlow

--- source/flow.cpp
+++ source/flow.cpp
@@ -4,16 +4,16 @@
 
 namespace sim {
 
-IFlow::IFlow(ISender *a_src, IReceiver *a_dest, uint32_t a_packet_size)
+Flow::Flow(ISender *a_src, IReceiver *a_dest, uint32_t a_packet_size)
     : m_src(a_src), m_dest(a_dest), m_packet_size(a_packet_size) {}
 
-void IFlow::schedule_packet_generation(std::uint32_t time) {
+void Flow::schedule_packet_generation(std::uint32_t time) {
     auto generate_event_ptr = std::make_unique<Generate>(Generate(this));
     generate_event_ptr->time = time;
     Scheduler::get_instance().add(std::move(generate_event_ptr));
 }
 
-void IFlow::generate_packet() {
+void Flow::generate_packet() {
     sim::Packet packet;
     packet.type = sim::PacketType::DATA;
     packet.size = m_packet_size;
--- source/flow.hpp
+++ source/flow.hpp
@@ -7,24 +7,35 @@
 namespace sim {
 
 class IFlow {
+    public:
+        // Start at time
+        virtual void start(std::uint32_t time) = 0;
+
+        // Try to generate a new packet if the internal state allows to do so.
+        // Returns true if a generate_event was created and placed in scheduler
+        virtual bool try_to_generate(std::uint32_t packet_size) = 0;
+    
+        // Update the internal state according to some congestion control algorithm
+        // Call try_to_generate upon the update
+        virtual void update() = 0;
+
+        virtual ISender* get_sender() const noexcept = 0;
+        virtual IReceiver* get_receiver() const noexcept = 0;
+    
+        virtual ~IFlow() = default;
+    };
+
+class Flow : IFlow {
 public:
-    virtual ~IFlow() = default;
-
-    IFlow(ISender *a_src, IReceiver *a_dest, uint32_t a_packet_size);
-
-    // Start at time
+    Flow(ISender *a_src, IReceiver *a_dest, uint32_t a_packet_size);
     virtual void start(std::uint32_t time) = 0;
-
-    // Try to generate a new packet if the internal state allows to do so.
-    // Returns true if a generate_event was created and placed in scheduler
     virtual bool try_to_generate(std::uint32_t current_time) = 0;
-
-    // Update flow state based on delay
     virtual void update(std::uint32_t delay) = 0;
 
     ISender *m_src;
     IReceiver *m_dest;
 
+    virtual ~Flow() = default;
 protected:
     void schedule_packet_generation(std::uint32_t time);
     void generate_packet();
