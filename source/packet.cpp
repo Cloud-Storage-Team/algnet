7ef7afb2b1582283865dfe81c7033cce4b348aba
Артём Пешков <47276660+ArtyomPeshkov@users.noreply.github.com>
2025-04-07 14:05:46 +0300

    Update source/packet.cpp
    Co-authored-by: Павел Ральников <112890673+PaulRalnikov@users.noreply.github.com>

--- source/packet.cpp
+++ source/packet.cpp
@@ -5,6 +5,9 @@ Packet::Packet(PacketType a_type, std::uint32_t a_size, IFlow* a_flow)
     : type(a_type), size(a_size), flow(a_flow) {}
 
 std::shared_ptr<IRoutingDevice> Packet::get_destination() const {
+    if (flow == nullptr) {
+        return nullptr;
+    }
     switch (type)
     {
     case ACK:
