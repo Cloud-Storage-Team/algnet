cf49425808ad3eff6d76d8049f73bf4fd83d1e81
Ivan Shanygin <88805084+AntoxaBarin@users.noreply.github.com>
2025-04-09 01:06:20 +0300

    Replace std::uint32_t with Time in Simulator::start()
    Co-authored-by: Артём Пешков <47276660+ArtyomPeshkov@users.noreply.github.com>

--- source/simulator.cpp
+++ source/simulator.cpp
@@ -113,7 +113,7 @@ void Simulator::recalculate_paths() {
     }
 }
 
-void Simulator::start(std::uint32_t a_stop_time) {
+void Simulator::start(Time a_stop_time) {
     recalculate_paths();
     m_scheduler.add(std::make_unique<Stop>(a_stop_time));
     for (std::shared_ptr<IFlow> flow : m_flows) {
