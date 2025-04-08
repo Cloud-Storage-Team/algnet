2c4bf3abc8d8691df8eb0faf437366118995f0e7
Ivan Shanygin <shanygin101@yandex.ru>
2025-04-09 02:39:04 +0300

    Simplify BFS

--- source/simulator.cpp
+++ source/simulator.cpp
@@ -12,13 +12,16 @@
 
 namespace sim {
 
+using routing_table_t =
+    std::unordered_map<std::shared_ptr<IRoutingDevice>, std::shared_ptr<ILink>>;
+
 Simulator::Simulator() : m_scheduler(Scheduler::get_instance()) {}
 
 std::shared_ptr<IRoutingDevice> Simulator::add_device(std::string a_name,
                                                       DeviceType a_type) {
     if (m_graph.find(a_name) != m_graph.end()) {
         LOG_WARN("add_device failed: device with name {} already exists.",
-                     a_name);
+                 a_name);
         return nullptr;
     }
     switch (a_type) {
@@ -43,8 +46,8 @@ void Simulator::add_flow(std::shared_ptr<IRoutingDevice> a_from,
         LOG_WARN("add_flow failed: sender is nullptr");
         return;
     }
-    
-    if (receiver== nullptr) {
+
+    if (receiver == nullptr) {
         LOG_WARN("add_flow failed: receiver is nullptr");
         return;
     }
@@ -62,10 +65,8 @@ void Simulator::add_link(std::shared_ptr<IRoutingDevice> a_from,
 
 // returns map, that for each meet device gives link to its parent in bfs bypass
 // tree
-std::unordered_map<std::shared_ptr<IRoutingDevice>, std::shared_ptr<ILink>> bfs(
-    std::shared_ptr<IRoutingDevice> start_device) {
-    std::unordered_map<std::shared_ptr<IRoutingDevice>, std::shared_ptr<ILink>>
-        parent_table;
+routing_table_t bfs(std::shared_ptr<IRoutingDevice>& start_device) {
+    routing_table_t routing_table;
     std::queue<std::shared_ptr<IRoutingDevice>> queue;
     std::set<std::shared_ptr<IRoutingDevice>> used;
     queue.push(start_device);
@@ -82,33 +83,24 @@ std::unordered_map<std::shared_ptr<IRoutingDevice>, std::shared_ptr<ILink>> bfs(
             if (used.find(link->get_to()) != used.end()) {
                 continue;
             }
-            if (!parent_table.contains(link->get_to())) {
-                parent_table[link->get_to()] = link;
+            auto next_hop = link->get_to();
+            if (link->get_from() == start_device) {
+                routing_table[next_hop] = link;
+            } else if (!routing_table.contains(next_hop)) {
+                routing_table[next_hop] = routing_table[link->get_from()];
             }
-            queue.push(link->get_to());
+            queue.push(next_hop);
         }
     }
-    return parent_table;
+    return routing_table;
 };
 
 void Simulator::recalculate_paths() {
     for (auto& [src_device_name, src_device] : m_graph) {
-        std::unordered_map<std::shared_ptr<IRoutingDevice>,
-                           std::shared_ptr<ILink>>
-            parent_table = bfs(src_device);
-        for (auto& [dest_device_name, dest_device] : m_graph) {
-            if (parent_table.find(dest_device) == parent_table.end()) {
-                src_device->update_routing_table(dest_device, nullptr);
-                continue;
-            }
-            std::shared_ptr<IRoutingDevice> next_hop = dest_device;
-            while (parent_table[next_hop]->get_from() != src_device) {
-                next_hop = parent_table[next_hop]->get_from();
-            }
-            bool status = src_device->update_routing_table(
-                dest_device, parent_table[next_hop]);
-            LOG_INFO("Update routing table {} --> {}: {}", src_device_name,
-                         dest_device_name, (status ? "true" : "false"));
+        routing_table_t routing_table = bfs(src_device);
+
+        for (auto [dest_device, link] : routing_table) {
+            src_device->update_routing_table(dest_device, link);
         }
     }
 }
