54c3e00584e4cdb6122b85c00af644c819516d00
Ivan Shanygin <shanygin101@yandex.ru>
2025-04-09 12:40:55 +0300

    Merge inlink and outlink tests

--- test/device/add_link_test.cpp
+++ test/device/add_link_test.cpp
@@ -2,6 +2,9 @@
 
 #include <random>
 
+#include "device.hpp"
+#include "link.hpp"
+#include "logger.hpp"
 #include "utils.hpp"
 
 namespace test {
@@ -12,7 +15,23 @@ public:
     void SetUp() override {};
 };
 
-TEST_F(AddLink, InlinkIsPresent) {
+std::shared_ptr<sim::ILink> next_outlink(
+    std::shared_ptr<sim::IRoutingDevice> device) {
+    static auto outlinks = device->get_outlinks();
+    static auto next_outlink_it = outlinks.begin();
+
+    if (outlinks.empty()) {
+        return nullptr;
+    }
+
+    std::shared_ptr<sim::ILink> link = *next_outlink_it;
+    if (++next_outlink_it == outlinks.end()) {
+        next_outlink_it = outlinks.begin();
+    }
+    return link;
+}
+
+TEST_F(AddLink, LinkIsPresent) {
     auto source = std::make_shared<sim::RoutingModule>(sim::RoutingModule());
     auto dest = std::make_shared<sim::RoutingModule>(sim::RoutingModule());
     auto link = std::make_shared<TestLink>(TestLink(source, dest));
@@ -20,46 +39,6 @@ TEST_F(AddLink, InlinkIsPresent) {
     EXPECT_EQ(dest->next_inlink(), nullptr);
     dest->add_inlink(link);
     EXPECT_EQ(dest->next_inlink(), link);
-}
-
-TEST_F(AddLink, SameInlinkMultipleTimes) {
-    int NUMBER_OF_SOURCES = 5;
-    int MAX_LINKS = 3;
-    int NUMBER_OF_LOOPS = 3;
-
-    auto sources = createRoutingModules(NUMBER_OF_SOURCES);
-    auto dest = std::make_shared<sim::RoutingModule>(sim::RoutingModule());
-
-    std::mt19937 gen(RANDOM_SEED);
-    std::uniform_int_distribution<> dis(1, MAX_LINKS);
-
-    std::unordered_map<std::shared_ptr<sim::ILink>, int> number_of_appearances;
-    for (auto source : sources) {
-        auto link = std::make_shared<TestLink>(source, dest);
-
-        int links_to_add = dis(gen);
-        for (size_t i = 0; i < links_to_add; ++i) {
-            dest->add_inlink(link);
-        }
-        number_of_appearances[link] = 0;
-    }
-
-    for (size_t loop = 0; loop < NUMBER_OF_LOOPS; loop++) {
-        for (size_t i = 0; i < NUMBER_OF_SOURCES; i++) {
-            auto current_link = dest->next_inlink();
-            number_of_appearances[current_link]++;
-        }
-    }
-
-    for (auto appearances : number_of_appearances) {
-        EXPECT_EQ(appearances.second, NUMBER_OF_LOOPS);
-    }
-}
-
-TEST_F(AddLink, OutlinkIsPresent) {
-    auto source = std::make_shared<sim::RoutingModule>(sim::RoutingModule());
-    auto dest = std::make_shared<sim::RoutingModule>(sim::RoutingModule());
-    auto link = std::make_shared<TestLink>(TestLink(source, dest));
 
     EXPECT_TRUE(source->get_outlinks().empty());
     source->add_outlink(link);
@@ -68,42 +47,46 @@ TEST_F(AddLink, OutlinkIsPresent) {
     EXPECT_EQ(*outlinks.begin(), link);
 }
 
-TEST_F(AddLink, SameOutlinkMultipleTimes) {
-    int NUMBER_OF_DESTINATIONS = 5;
+TEST_F(AddLink, SameLinkMultipleTimes) {
+    int NUMBER_OF_NEIGHBOURS = 5;
     int MAX_LINKS = 3;
     int NUMBER_OF_LOOPS = 3;
 
-    auto destinations = createRoutingModules(NUMBER_OF_DESTINATIONS);
+    auto neighbours = createRoutingModules(NUMBER_OF_NEIGHBOURS);
+    auto dest = std::make_shared<sim::RoutingModule>(sim::RoutingModule());
     auto source = std::make_shared<sim::RoutingModule>(sim::RoutingModule());
 
     std::mt19937 gen(RANDOM_SEED);
     std::uniform_int_distribution<> dis(1, MAX_LINKS);
 
-    std::unordered_map<std::shared_ptr<sim::ILink>, int> number_of_appearances;
-    for (auto dest : destinations) {
-        auto link = std::make_shared<TestLink>(source, dest);
+    std::unordered_map<std::shared_ptr<sim::ILink>, int> number_of_inlink_appearances;
+    std::unordered_map<std::shared_ptr<sim::ILink>, int> number_of_outlink_appearances;
+    
+    for (auto device : neighbours) {
+        auto inlink = std::make_shared<TestLink>(device, dest);
+        auto outlink = std::make_shared<TestLink>(source, device);
 
         int links_to_add = dis(gen);
         for (size_t i = 0; i < links_to_add; ++i) {
-            source->add_outlink(link);
+            dest->add_inlink(inlink);
+            source->add_outlink(outlink);
         }
-        number_of_appearances[link] = 0;
+        number_of_inlink_appearances[inlink] = 0;
+        number_of_outlink_appearances[outlink] = 0;
     }
-
-    auto outlinks = source->get_outlinks();
-    auto next_outlink = outlinks.begin();
+    
     for (size_t loop = 0; loop < NUMBER_OF_LOOPS; loop++) {
-        for (size_t i = 0; i < NUMBER_OF_DESTINATIONS; i++) {
-            auto current_link = *next_outlink;
-            number_of_appearances[current_link]++;
-            next_outlink++;
-            if (next_outlink == outlinks.end()) {
-                next_outlink = outlinks.begin();
-            }
+        for (size_t i = 0; i < NUMBER_OF_NEIGHBOURS; i++) {
+            auto current_inlink = dest->next_inlink();
+            number_of_inlink_appearances[current_inlink]++;
+            auto current_outlink = next_outlink(source);
+            number_of_outlink_appearances[current_outlink]++;
         }
     }
-
-    for (auto appearances : number_of_appearances) {
+    for (auto appearances : number_of_inlink_appearances) {
+        EXPECT_EQ(appearances.second, NUMBER_OF_LOOPS);
+    }
+    for (auto appearances : number_of_outlink_appearances) {
         EXPECT_EQ(appearances.second, NUMBER_OF_LOOPS);
     }
 }
