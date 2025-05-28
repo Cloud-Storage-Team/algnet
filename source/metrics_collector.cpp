0288041b5ce0aa7b9bc5fdee0735a25c2a660b1a
Ivan Shanygin <shanygin101@yandex.ru>
2025-05-29 01:51:24 +0300

    Add link egress buffer size on plot

--- source/metrics_collector.cpp
+++ source/metrics_collector.cpp
@@ -112,10 +112,15 @@ void MetricsCollector::draw_metric_plots() const {
         ax->plot(x_data, y_data, "-o")->line_width(1.5);
 
         ax->xlabel("Time, ns");
-        ax->ylabel("Value, packets");
+        ax->ylabel("Value, bytes");
 
         auto link =
             IdentifierFactory::get_instance().get_object<ILink>(link_id);
+
+        matplot::line(0, link->get_max_src_egress_buffer_size_byte(), 10000,
+                      link->get_max_src_egress_buffer_size_byte())
+            ->line_width(1.5)
+            .color({1.f, 0.0f, 0.0f});
 
         ax->title(fmt::format("Queue size from {} to {}",
                               link->get_from()->get_id(),
