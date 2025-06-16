960293c3019fad2a3727bf896e1217a17e8ea6c0
Pavel Ralnikov <ralnikovp@gmail.com>
2025-06-16 16:49:28 +0300

    Renamed algorithms

--- source/simulator.cpp
+++ source/simulator.cpp
@@ -5,10 +5,10 @@
 namespace sim {
 
 SimulatorVariant create_simulator(std::string_view algorithm) {
-    if (algorithm == "new_basic") {
+    if (algorithm == "basic") {
         return BasicSimulator();
     }
-    if (algorithm == "new_tcp") {
+    if (algorithm == "tcp") {
         return TcpSimulator();
     }
     throw std::invalid_argument("Unknown algorithm: " + std::string(algorithm));
