8dbccde17f88863166ca718a4459d3e1deb31899
Pavel Ralnikov <ralnikovp@gmail.com>
2025-06-27 18:01:04 +0300

    Added SCHEDULER compile option usage

--- source/scheduler/scheduler.hpp
+++ source/scheduler/scheduler.hpp
@@ -10,7 +10,15 @@ namespace sim {
 
 // Scheduler is implemented as a Singleton class
 // which provides a global access to a single instance
-class Scheduler : public SimpleScheduler {
+class Scheduler : public
+#if !defined(SCHEDULER)
+                  SimpleScheduler
+#elif SCHEDULER = ASYNC
+                  MultithreadScheduler
+#else
+                  SimpleScheduler
+#endif
+{
 public:
     // Static method to get the instance
     static Scheduler& get_instance() {
@@ -24,6 +32,6 @@ private:
     // No copy constructor and assignment operators
     Scheduler(const Scheduler&) = delete;
     Scheduler& operator=(const Scheduler&) = delete;
-};
+};  // namespace sim
 
 }  // namespace sim
