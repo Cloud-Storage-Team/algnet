d0f6c0b2f20f151b49edb05ee59d3dfcc265d1ce
Pavel Ralnikov <ralnikovp@gmail.com>
2025-07-03 16:35:35 +0300

    Writes metric in separate thread

--- source/metrics/multi_id_metrics_collector.cpp
+++ source/metrics/multi_id_metrics_collector.cpp
@@ -4,25 +4,31 @@
 
 namespace sim {
 MultiIdMetricsStorage::MultiIdMetricsStorage(std::string a_metric_name)
-    : metric_name(std::move(a_metric_name)) {}
+    : metric_name(std::move(a_metric_name)) {
+    m_needs_write.store(true);
+    m_writer = std::thread(&MultiIdMetricsStorage::writer_cycle, this);
+}
+
+MultiIdMetricsStorage::~MultiIdMetricsStorage() {
+    m_needs_write.store(false);
+    {
+        std::lock_guard lock(m_mutex);
+        m_condvar.notify_all();
+    }
+    if (m_writer.joinable()) {
+        m_writer.join();
+    }
+}
 
 void MultiIdMetricsStorage::add_record(Id id, Time time, double value) {
-    auto it = m_storage.find(id);
-    if (it == m_storage.end()) {
-        // if (!std::regex_match(get_metrics_filename(id), m_filter)) {
-        //     m_storage[id] = std::nullopt;
-        // } else {
-        MetricsStorage new_storage;
-        new_storage.add_record(time, value);
-        m_storage.emplace(id, std::move(new_storage));
-        // }
-    } else /*if (it->second.has_value())*/ {
-        it->second->add_record(time, value);
-    }
+    std::lock_guard lock(m_mutex);
+    m_record_queue.emplace(std::move(id), std::move(time), std::move(value));
+    m_condvar.notify_all();
 }
 
 void MultiIdMetricsStorage::export_to_files(
     std::filesystem::path output_dir_path) const {
+    std::lock_guard lock(m_mutex);
     for (auto& [id, values] : m_storage) {
         if (values) {
             values->export_to_file(output_dir_path / get_metrics_filename(id));
@@ -32,6 +38,7 @@ void MultiIdMetricsStorage::export_to_files(
 
 std::unordered_map<Id, MetricsStorage> MultiIdMetricsStorage::data() const {
     std::unordered_map<Id, MetricsStorage> result;
+    std::lock_guard lock(m_mutex);
     for (auto [id, maybe_storage] : m_storage) {
         if (maybe_storage) {
             result[id] = maybe_storage.value();
@@ -46,6 +53,35 @@ void MultiIdMetricsStorage::set_filter(std::string filter) {
 
 std::string MultiIdMetricsStorage::get_metrics_filename(Id id) const {
     return fmt::format("{}/{}.txt", metric_name, id);
+}
+
+void MultiIdMetricsStorage::writer_cycle() {
+    while (m_needs_write) {
+        std::unique_lock lock(m_mutex);
+        m_condvar.wait(lock, [this]() {
+            return !m_record_queue.empty() || !m_needs_write;
+        });
+
+        if (!m_needs_write) {
+            break;
+        }
+
+        auto [id, time, value] = m_record_queue.front();
+        m_record_queue.pop();
+
+        auto it = m_storage.find(id);
+        if (it == m_storage.end()) {
+            if (!std::regex_match(get_metrics_filename(id), m_filter)) {
+                m_storage[id] = std::nullopt;
+            } else {
+                MetricsStorage new_storage;
+                new_storage.add_record(time, value);
+                m_storage.emplace(id, std::move(new_storage));
+            }
+        } else if (it->second.has_value()) {
+            it->second->add_record(time, value);
+        }
+    }
 }
 
 }  // namespace sim
--- source/metrics/multi_id_metrics_storage.hpp
+++ source/metrics/multi_id_metrics_storage.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include <spdlog/fmt/fmt.h>
 
+#include <condition_variable>
+#include <mutex>
+#include <queue>
 #include <regex>
-#include <type_traits>
+#include <thread>
 
 #include "metrics_storage.hpp"
 
@@ -10,6 +13,7 @@ namespace sim {
 class MultiIdMetricsStorage {
 public:
     MultiIdMetricsStorage(std::string a_metric_name);
+    ~MultiIdMetricsStorage();
 
     void add_record(Id id, Time time, double value);
     void export_to_files(std::filesystem::path output_dir_path) const;
@@ -20,13 +24,25 @@ public:
 
 private:
     std::string get_metrics_filename(Id id) const;
+    void writer_cycle();
 
     std::string metric_name;
+
+    mutable std::mutex m_mutex;         // Protecs m_storage and m_record_queue
+    std::condition_variable m_condvar;  // If m_record_queue is not empty
+
+    std::queue<std::tuple<Id, Time, double> >
+        m_record_queue;  // queue of metrics records
+
     // If m_storage does not contain some id, there was no check is metrics file
     // name for id correspond to m_filter
     // If m_storage[id] = std::nullopt, this check was failed
     // Otherwice, check was succseed
     std::unordered_map<Id, std::optional<MetricsStorage> > m_storage;
     std::regex m_filter;
+
+    std::atomic<bool> m_needs_write;
+    std::thread m_writer;
 };
+
 }  // namespace sim
