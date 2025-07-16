b289190905578277380772654f00030e1b4babe6
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-07-16 13:01:38 +0300

    done

--- source/parser/identifiable_parser/identifiable_parser.hpp
+++ source/parser/identifiable_parser/identifiable_parser.hpp
@@ -9,14 +9,14 @@
 
 namespace sim {
 
-template <typename T>
-class IdentifieableParser {
+template <typename TIP, typename T>
+class IdentifieableParserCommon {
     static_assert(std::is_base_of_v<Identifiable, T>);
 
 public:
     static std::shared_ptr<T> parse_and_registrate(
         const YAML::Node& key_node, const YAML::Node& value_node) {
-        std::shared_ptr<T> object = parse_object(key_node, value_node);
+        std::shared_ptr<T> object = TIP::parse_object(key_node, value_node);
         if (!IdentifierFactory::get_instance().add_object(object)) {
             throw std::runtime_error(fmt::format(
                 "Can not add object with type {}; object with same id ({}) "
@@ -25,8 +25,11 @@ public:
         }
         return object;
     }
+};
 
-private:
+template <typename T>
+class IdentifieableParser: public IdentifieableParserCommon<IdentifieableParser<T>, T> {
+public:
     // Parses object and return shared_ptr to it
     static std::shared_ptr<T> parse_object(const YAML::Node& key_node,
                                            const YAML::Node& value_node);
