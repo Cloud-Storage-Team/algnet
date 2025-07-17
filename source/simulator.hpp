eb435514feee3b0b7d6b600ce15b1293e9774729
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-07-18 01:36:28 +0300

    fix

--- source/simulator.hpp
+++ source/simulator.hpp
@@ -82,7 +82,7 @@ public:
     // Calls BFS for each device to build the routing table
     void recalculate_paths() {
         for (auto src_device : get_devices()) {
-            RoutingTable routing_table = bfs(src_device);
+            RoutingTable routing_table = bfs(dynamic_pointer_cast<IRoutingDevice>(src_device));
             for (auto [dest_device_id, links] : routing_table) {
                 for (auto [link, paths_count] : links) {
                     src_device->update_routing_table(dest_device_id,
