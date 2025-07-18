4ddb3dbae44ca57a8efd2d4ccc3a5922696efcd6
Pavel Ralnikov <ralnikovp@gmail.com>
2025-07-18 16:40:45 +0300

    Initial version

--- source/types.hpp
+++ source/types.hpp
@@ -3,10 +3,14 @@
 #include <memory>
 #include <string>
 
+#include "units.hpp"
+
+namespace sim {
+
 // nanoseconds
 using Time = std::uint32_t;
 // bytes
-using Size = std::uint32_t;
+using Size = TemplateSize<Byte>;
 using Id = std::string;
 
 // Describes a type used by packet's bitset to store flags
@@ -15,3 +19,5 @@ using PacketFlagsBase = std::uint32_t;
 template <typename K, typename V>
 using MapWeakPtr =
     std::map<std::weak_ptr<K>, V, std::owner_less<std::weak_ptr<K>>>;
+
+}  // namespace sim
---
+++ source/units.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <concepts>
+#include <cstdint>
+#include <type_traits>
+
+namespace sim {
+
+struct Bit {
+    static constexpr uint64_t to_bit_multiplier = 1;
+};
+
+struct Byte {
+    static constexpr uint64_t to_bit_multiplier = (1ull << 3);
+};
+
+struct KBit {
+    static constexpr uint64_t to_bit_multiplier = (1ull << 10);
+};
+
+struct KByte {
+    static constexpr uint64_t to_bit_multiplier = (1ull << 13);
+};
+
+struct GBit {
+    static constexpr uint64_t to_bit_multiplier = (1ull << 20);
+};
+
+struct GByte {
+    static constexpr uint64_t to_bit_multiplier = (1ull << 23);
+};
+
+template <typename T>
+concept HasToBitMultiplier = requires {
+    { T::to_bit_multiplier } -> std::convertible_to<uint64_t>;
+};
+
+template <HasToBitMultiplier TSizeBase>
+class TemplateSize {
+public:
+    using Size = TemplateSize<TSizeBase>;
+
+    // Attention: a_value given in TSizeByte units!
+    TemplateSize(uint64_t a_value)
+        : m_value_bits(a_value * TSizeBase::to_bit_multiplier) {}
+
+    operator uint64_t() const {
+        return m_value_bits / TSizeBase::to_bit_multiplier;
+    }
+
+    void operator+=(Size size) { m_value_bits += size.m_value_bits; }
+    void operator-=(Size size) { m_value_bits -= size.m_value_bits; }
+
+private:
+    uint64_t m_value_bits;  // Size in bits
+};
+
+}  // namespace sim
