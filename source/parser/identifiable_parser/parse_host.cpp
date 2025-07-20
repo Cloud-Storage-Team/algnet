e07a5a24bdffcc9863077b853e8464caa0f03867
Ivan Shanygin <shanygin101@yandex.ru>
2025-07-20 23:36:27 +0300

    Minor fixes

--- source/parser/identifiable_parser/parse_host.cpp
+++ source/parser/identifiable_parser/parse_host.cpp
@@ -12,7 +12,7 @@ std::shared_ptr<Host> Parser<Host>::parse_object(const YAML::Node& key_node,
                                                  const YAML::Node& value_node) {
     std::unique_ptr<IHasher> hasher = nullptr;
     if (value_node["hasher"]) {
-        std::unique_ptr<IHasher> hasher = parse_hasher(value_node);
+        hasher = parse_hasher(value_node);
     }
     return std::make_shared<Host>(key_node.as<Id>(), std::move(hasher));
 }
