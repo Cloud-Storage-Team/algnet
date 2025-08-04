98b19320fc9989f20421b67f3d2ed0a9448b11c4
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-08-04 21:31:51 +0300

    fix

--- source/simulator.hpp
+++ source/simulator.hpp
@@ -22,86 +22,23 @@ public:
     Simulator() = default;
     ~Simulator() = default;
 
-    bool add_host(std::shared_ptr<IHost> host) {
-        if (host == nullptr) {
-            return false;
-        }
-        return m_hosts.insert(host).second;
-    }
+    bool add_host(std::shared_ptr<IHost> host);
 
-    bool add_switch(std::shared_ptr<ISwitch> switch_device) {
-        if (switch_device == nullptr) {
-            return false;
-        }
-        return m_switches.insert(switch_device).second;
-    }
+    bool add_switch(std::shared_ptr<ISwitch> switch_device);
 
-    bool add_flow(std::shared_ptr<IFlow> flow) {
-        if (flow == nullptr) {
-            return false;
-        }
-        return m_flows.insert(flow).second;
-    }
+    bool add_flow(std::shared_ptr<IFlow> flow);
 
-    bool add_link(std::shared_ptr<ILink> link) {
-        if (!is_valid_link(link)) {
-            return false;
-        }
-        if (!m_links.insert(link).second) {
-            return false;
-        }
-        auto a_from = link->get_from();
-        auto a_to = link->get_to();
-        a_from->add_outlink(link);
-        a_to->add_inlink(link);
-        return true;
-    }
+    bool add_link(std::shared_ptr<ILink> link);
 
-    std::vector<std::shared_ptr<IDevice>> get_devices() const {
-        std::vector<std::shared_ptr<IDevice>> devices;
-
-        for (auto host : m_hosts) {
-            devices.push_back(dynamic_pointer_cast<IDevice>(host));
-        }
-
-        for (auto swtch : m_switches) {
-            devices.push_back(dynamic_pointer_cast<IDevice>(swtch));
-        }
-
-        return devices;
-    }
+    std::vector<std::shared_ptr<IDevice>> get_devices() const;
 
     // Calls BFS for each device to build the routing table
-    void recalculate_paths() {
-        for (auto src_device : get_devices()) {
-            RoutingTable routing_table =
-                bfs(dynamic_pointer_cast<IRoutingDevice>(src_device));
-            for (auto [dest_device_id, links] : routing_table) {
-                for (auto [link, paths_count] : links) {
-                    src_device->update_routing_table(dest_device_id,
-                                                     link.lock(), paths_count);
-                }
-            }
-        }
-    }
+    void recalculate_paths();
     // Create a Stop event at a_stop_time and start simulation
-    void start(TimeNs a_stop_time) {
-        recalculate_paths();
-        Scheduler::get_instance().add<Stop>(a_stop_time);
-        constexpr TimeNs start_time = TimeNs(0);
-
-        for (auto flow : m_flows) {
-            Scheduler::get_instance().add<StartFlow>(start_time, flow);
-        }
-
-        while (Scheduler::get_instance().tick()) {
-        }
-    }
+    void start(TimeNs a_stop_time);
 
     // returns summary in format [flow : size of delivered data]
-    std::unordered_set<std::shared_ptr<IFlow>> get_flows() const {
-        return m_flows;
-    }
+    std::unordered_set<std::shared_ptr<IFlow>> get_flows() const;
 
 private:
     std::unordered_set<std::shared_ptr<IHost>> m_hosts;
