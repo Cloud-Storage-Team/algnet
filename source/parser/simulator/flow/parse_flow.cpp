b11c5c13676bf58a2764c68b48a73b2d3a6e29ed
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-08-05 13:33:32 +0300

    fix

--- source/parser/simulator/flow/parse_flow.cpp
+++ source/parser/simulator/flow/parse_flow.cpp
@@ -22,13 +22,13 @@ std::unique_ptr<ITcpCC> ParseFlow::ParseTcpCC::parse_i_tcp_cc(Id flow_id, const 
         throw std::runtime_error("Missing 'cc.type' field in flow " + flow_id);
     }
 
-    std::string type = value_node["cc"]["type"].as<std::string>();
+    std::string type = value_node["type"].as<std::string>();
     if (type == "basic") {
         return std::make_unique<BasicCC>();
     } else if (type == "tahoe") {
         return std::make_unique<TcpTahoeCC>();
     } else if (type == "swift") {
-        TimeNs a_base_target = parse_time(value_node["cc"]["base_target"].as<std::string>());
+        TimeNs a_base_target = parse_time(value_node["base_target"].as<std::string>());
         return std::make_unique<TcpSwiftCC>(a_base_target);
     }
     throw std::runtime_error(fmt::format("Unexpected type of CC module: {}", type));
