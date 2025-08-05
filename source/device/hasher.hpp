8b960ffb002fd2dbd444274f2604bda19bc429b1
Pavel Ralnikov <ralnikovp@gmail.com>
2025-08-05 10:28:15 +0300

    Add device id to get_hash arguments

--- source/device/hasher.hpp
+++ source/device/hasher.hpp
@@ -9,28 +9,28 @@ namespace sim {
 class IPacketHasher {
 public:
     virtual ~IPacketHasher() = default;
-    virtual std::uint32_t get_hash(Packet packet) const = 0;
+    virtual std::uint32_t get_hash(Packet packet, Id deivce_id) const = 0;
 };
 
 class RandomHasher : public IPacketHasher {
 public:
     ~RandomHasher() = default;
 
-    std::uint32_t get_hash(Packet packet) const final;
+    std::uint32_t get_hash(Packet packet, Id deivce_id) const final;
 };
 
 class BaseHasher : public IPacketHasher {
 public:
     ~BaseHasher() = default;
 
-    std::uint32_t get_hash(Packet packet) const final;
+    std::uint32_t get_hash(Packet packet, Id deivce_id) const final;
 };
 
 class SymmetricHasher : public IPacketHasher {
 public:
     ~SymmetricHasher() = default;
 
-    std::uint32_t get_hash(Packet packet) const final;
+    std::uint32_t get_hash(Packet packet, Id deivce_id) const final;
 };
 
-} // namespace sim
+}  // namespace sim
