28ee24b640ab7a25b1978fc0d9dec4de46351266
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-08-05 10:53:16 +0300

    fix

--- source/parser/simulator/flow/parse_flow.cpp
+++ source/parser/simulator/flow/parse_flow.cpp
@@ -31,7 +31,7 @@ std::unique_ptr<ITcpCC> ParseFlow::ParseTcpCC::parse_i_tcp_cc(Id flow_id, const 
         TimeNs a_base_target = parse_time(value_node["cc"]["base_target"].as<std::string>());
         return std::make_unique<TcpSwiftCC>(a_base_target);
     }
-    throw std::runtime_errror(fmt::format("Unexpected type of CC module: {}", type));
+    throw std::runtime_error(fmt::format("Unexpected type of CC module: {}", type));
 }
 
 }  // namespace sim
