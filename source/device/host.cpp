3ef9a76fee56fa2caa222211acf002027eb06f31
Pavel Ralnikov <ralnikovp@gmail.com>
2025-08-05 16:57:51 +0300

    Added a_packet_hasher argument to host & switch constructors

--- source/device/host.cpp
+++ source/device/host.cpp
@@ -7,7 +7,8 @@
 
 namespace sim {
 
-Host::Host(Id a_id) : RoutingModule(a_id) {}
+Host::Host(Id a_id, std::unique_ptr<IPacketHasher> a_packet_hasher)
+    : RoutingModule(a_id, std::move(a_packet_hasher)) {}
 
 bool Host::notify_about_arrival(TimeNs arrival_time) {
     return m_process_scheduler.notify_about_arriving(arrival_time,
