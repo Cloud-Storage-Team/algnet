3b40a3fafdd986e172f1938361bb853e24dab151
Артём Пешков <47276660+ArtyomPeshkov@users.noreply.github.com>
2025-08-05 10:28:15 +0300

    Update source/parser/identifiable_parser/flow/parse_tcp_flow.cpp
    Co-authored-by: Павел Ральников <112890673+PaulRalnikov@users.noreply.github.com>

--- source/parser/identifiable_parser/flow/parse_tcp_flow.cpp
+++ source/parser/identifiable_parser/flow/parse_tcp_flow.cpp
@@ -36,7 +36,7 @@ std::shared_ptr<TcpFlow> ParseFlow::parse_tcp_flow(const YAML::Node& key_node,
         value_node["number_of_packets"].as<std::uint32_t>();
 
     return std::make_shared<TcpFlow>(
-        id, sender_ptr, receiver_ptr, std::move(cc), packet_size, number_of_packets);                                                   
+        id, sender_ptr, receiver_ptr, std::move(cc), packet_size, number_of_packets);
 }
 
 }  // namespace sim
