09529be4d82ab3eb76c39323773b4898d2262e43
Pavel Ralnikov <ralnikovp@gmail.com>
2025-08-05 11:30:35 +0300

    Remove const from IHasher::get_hash

--- source/device/hasher.cpp
+++ source/device/hasher.cpp
@@ -5,11 +5,11 @@
 
 namespace sim {
 
-std::uint32_t RandomHasher::get_hash(Packet, Id) const {
+std::uint32_t RandomHasher::get_hash(Packet, Id) {
     return static_cast<std::uint32_t>(std::rand());
 };
 
-std::uint32_t ECMPHasher::get_hash(Packet packet, Id) const {
+std::uint32_t ECMPHasher::get_hash(Packet packet, Id) {
     std::string flow_id_str =
         ((packet.flow == nullptr) ? "0" : packet.flow->get_id());
 
@@ -19,7 +19,7 @@ std::uint32_t ECMPHasher::get_hash(Packet packet, Id) const {
     return static_cast<uint32_t>(hasher(header_str));
 };
 
-std::uint32_t SymmetricHasher::get_hash(Packet packet, Id) const {
+std::uint32_t SymmetricHasher::get_hash(Packet packet, Id) {
     std::hash<std::string> hasher;
 
     std::string combined_id_str =
--- source/device/hasher.hpp
+++ source/device/hasher.hpp
@@ -9,28 +9,34 @@ namespace sim {
 class IPacketHasher {
 public:
     virtual ~IPacketHasher() = default;
-    virtual std::uint32_t get_hash(Packet packet, Id deivce_id) const = 0;
+    virtual std::uint32_t get_hash(Packet packet, Id deivce_id) = 0;
 };
 
 class RandomHasher : public IPacketHasher {
 public:
     ~RandomHasher() = default;
 
-    std::uint32_t get_hash(Packet packet, Id device_id) const final;
+    std::uint32_t get_hash(Packet packet, Id device_id) final;
 };
 
 class ECMPHasher : public IPacketHasher {
 public:
     ~ECMPHasher() = default;
 
-    std::uint32_t get_hash(Packet packet, Id device_id) const final;
+    std::uint32_t get_hash(Packet packet, Id device_id) final;
+};
+
+class FLowletHasher : public IPacketHasher {
+    ~FLowletHasher() = default;
+
+    std::uint32_t get_hash(Packet packet, Id device_id) final;
 };
 
 class SymmetricHasher : public IPacketHasher {
 public:
     ~SymmetricHasher() = default;
 
-    std::uint32_t get_hash(Packet packet, Id device_id) const final;
+    std::uint32_t get_hash(Packet packet, Id device_id) final;
 };
 
 }  // namespace sim
