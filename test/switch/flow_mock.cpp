5f3e8681c6017b1254876b2aee71479de8c59625
Alexey Zharkov <alexezharkov@yandex.ru>
2025-08-07 11:28:25 +0300

    restore tests

--- test/switch/flow_mock.cpp
+++ test/switch/flow_mock.cpp
@@ -7,8 +7,14 @@ FlowMock::FlowMock(std::shared_ptr<sim::IHost> m_receiver)
 
 void FlowMock::start() {}
 
-void FlowMock::update(sim::Packet packet) {};
+void FlowMock::update([[maybe_unused]] sim::Packet packet) {};
 
+bool FlowMock::can_send() const { return true; }
+void FlowMock::send_packet() {}
+void FlowMock::set_conn([[maybe_unused]] std::shared_ptr<sim::Connection> connection) {}
+std::shared_ptr<sim::Connection> FlowMock::get_conn() const {
+    return nullptr;
+}
 SizeByte FlowMock::get_delivered_data_size() const { return SizeByte(0); }
 
 std::shared_ptr<sim::IHost> FlowMock::get_sender() const { return nullptr; }
