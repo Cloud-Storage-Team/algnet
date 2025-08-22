e2ceb03c3ce28c2debbab3fd09dea1cec298d688
Dmitry R. Gulevich <drgulevich@gmail.com>
2025-08-22 11:26:00 +0300

    Update ds/test_stack.cpp
    Co-authored-by: Павел Ральников <112890673+PaulRalnikov@users.noreply.github.com>

--- ds/test_stack.cpp
+++ ds/test_stack.cpp
@@ -158,7 +158,7 @@ void testAssign() {
 }
 
 void testTopThrowsAnInvalidArgumentWhenStackEmpty() {
-    const stack<double> curStack;
+    const stack<long double> curStack;
     bool wasException = false;
     try {
         curStack.top();
