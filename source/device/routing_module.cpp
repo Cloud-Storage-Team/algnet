449380d70dfd6aed793a08d12635ca4dd4664b71
Pavel Ralnikov <ralnikovp@gmail.com>
2025-10-02 14:28:27 +0300

    Fix naming in routing module

--- source/device/routing_module.cpp
+++ source/device/routing_module.cpp
@@ -91,12 +91,12 @@ std::shared_ptr<ILink> RoutingModule::get_link_to_destination(
         total_weight += weight;
     }
 
-    int random_value = m_hasher->get_hash(packet) % total_weight;
+    int hash = m_hasher->get_hash(packet) % total_weight;
 
     int cumulative_weight = 0;
     for (const auto& [link, weight] : link_map) {
         cumulative_weight += weight;
-        if (random_value < cumulative_weight) {
+        if (hash < cumulative_weight) {
             return link.lock();
         }
     }
