1c26f403a43d449044d5fa0da541dd63f802c0e2
Павел Ральников <112890673+PaulRalnikov@users.noreply.github.com>
2025-10-08 10:25:12 +0300

    Update source/parser/config_reader/node_stacktrace.cpp
    Co-authored-by: Alexey Zharkov <62723911+Az3git@users.noreply.github.com>

--- source/parser/config_reader/node_stacktrace.cpp
+++ source/parser/config_reader/node_stacktrace.cpp
@@ -63,7 +63,7 @@ NodeStacktracePtr NodeStacktraceFactory::create_root() {
 
 NodeStacktracePtr NodeStacktraceFactory::create_from_parent(
     NodeStacktracePtr parent, std::string key) {
-    // used raw contsructor because std::make_shared can not be used
+    // used raw constructor because std::make_shared can not be used
     // (std::shared_ptr is not a friend of NodeStacktrace but uses private
     // constructor)
     return NodeStacktracePtr(new NodeStacktrace(parent, std::move(key)));
