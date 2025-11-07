40044585082ec8aad3eaae82b3d4687274ac1bfa
Artyom <artemyipeshkov@gmail.com>
2025-11-07 17:10:50 +0300

    fix

--- fix-metrics-collector/18907134885/flamegraph/source/device/hashers/random_hasher.cpp
+++
@@ -1,9 +0,0 @@
-#include "random_hasher.hpp"
-
-namespace sim {
-
-std::uint32_t RandomHasher::get_hash(const Packet&) {
-    return static_cast<std::uint32_t>(std::rand());
-}
-
-}  // namespace sim
