347f40c1336e58baba1dee9efa9b99d25247bf1f
Pavel Ralnikov <ralnikovp@gmail.com>
2025-11-27 17:38:23 +0300

    Add work piece of NewDataId

--- source/data.cpp
+++ source/data.cpp
@@ -3,6 +3,14 @@
 #include <spdlog/fmt/fmt.h>
 
 namespace sim {
+
+std::string NewDataId::to_string() const {
+    return fmt::format(
+        "(raw id: {}, repeat num: {})", raw_id,
+        (repeat_num.has_value() ? std::to_string(repeat_num.value())
+                                : "not set"));
+}
+
 Data::Data(DataId a_id, SizeByte a_size) : id(a_id), size(a_size) {}
 
 std::string Data::to_string() const {
--- source/data.hpp
+++ source/data.hpp
@@ -1,7 +1,22 @@
 #pragma once
+#include <optional>
+
 #include "types.hpp"
 
 namespace sim {
+
+// TODO: use NewDataId instead of DataId
+using RepeatNum = std::size_t;
+using RawDataId = std::string;
+
+struct NewDataId {
+    RawDataId raw_id = {};
+    std::optional<RepeatNum> repeat_num = std::nullopt;
+
+    auto operator<=>(const NewDataId&) const = default;
+
+    std::string to_string() const;
+};
 
 using DataId = std::string;
 
