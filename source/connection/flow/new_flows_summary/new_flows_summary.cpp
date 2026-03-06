2312b5017d2534285b91c72d29e190a7730de8df
Pavel Ralnikov <ralnikovp@gmail.com>
2026-03-06 18:25:07 +0300

    Changes by review

--- source/connection/flow/new_flows_summary/new_flows_summary.cpp
+++ source/connection/flow/new_flows_summary/new_flows_summary.cpp
@@ -15,14 +15,14 @@ void NewFlowsSummary::write_to_csv(std::filesystem::path output_path) const {
     if (!out) {
         throw std::runtime_error("Failed to create file for summary");
     }
-    out << "Flow id";
-    out << ", Sent size (bytes)";
-    out << ", Delivered size (bytes)";
-    out << ", Overhead (%)";
-    out << ", Retransmit size (bytes)";
-    out << ", Delivery rate (Gbps)";
-    out << ", FCT (ns)";
-    out << "\n";
+    out << "Flow id"
+        << ", Sent size (bytes)"
+        << ", Delivered size (bytes)"
+        << ", Overhead (%)"
+        << ", Retransmit size (bytes)"
+        << ", Effective delivery rate (Gbps)"
+        << ", Delivery rate (Gbps)"
+        << ", FCT (ns)\n";
     for (const auto& [flow_id, ctx] : m_flows_contexts) {
         TimeNs fct =
             (ctx.last_ack_receive_time.has_value() && ctx.start_time.has_value()
@@ -32,15 +32,24 @@ void NewFlowsSummary::write_to_csv(std::filesystem::path output_path) const {
         double overhead = ctx.delivered_size != SizeByte(0)
                               ? (ctx.sent_size / ctx.delivered_size - 1) * 100
                               : std::nan("");
+        // Effective delivery rate is average delivery rate calculated on the
+        // spot (when connection is active) so does not include incative time
+        // segments
+        SpeedGbps effective_delivery_rate =
+            ctx.delivery_rate_statistics.get_mean().value_or(SpeedGbps(0.0));
+
+        constexpr static double EPS = 1e-8;
 
         SpeedGbps delivery_rate =
-            ctx.delivery_rate_statistics.get_mean().value_or(SpeedGbps(0.0));
+            (std::fabs(fct.value()) > EPS ? SpeedGbps(ctx.delivered_size / fct)
+                                          : SpeedGbps(0.0));
 
         out << flow_id;
         out << ", " << ctx.sent_size;
         out << ", " << ctx.delivered_size;
         out << ", " << overhead;
         out << ", " << ctx.retransmit_size;
+        out << ", " << effective_delivery_rate;
         out << ", " << delivery_rate;
         out << ", " << fct;
         out << "\n";
--- source/connection/new_connections_summary/new_connections_summary.cpp
+++ source/connection/new_connections_summary/new_connections_summary.cpp
@@ -15,34 +15,35 @@ void NewConnectionsSummary::write_to_csv(
     utils::create_all_directories(output_path);
     std::ofstream out(output_path);
     if (!out) {
-        throw std::runtime_error(
-            fmt::format("Failed to create file for summary: {}", output_path.string()));
+        throw std::runtime_error(fmt::format(
+            "Failed to create file for summary: {}", output_path.string()));
     }
-    out << "Connection id";
-    out << ", Sent size (bytes)";
-    out << ", Delivered size (bytes)";
-    out << ", Effective delivery rate (Gbps)";
-    out << ", Delivery rate (Gbps)";
-    out << ", CCT (ns)";
-    out << '\n';
+    out << "Connection id"
+        << ", Sent size (bytes)"
+        << ", Delivered size (bytes)"
+        << ", Effective delivery rate (Gbps)"
+        << ", Delivery rate (Gbps)"
+        << ", CCT (ns)\n";
     for (const auto& [id, ctx] : m_contexts_table) {
+        // Effective delivery rate is average delivery rate calculated on the
+        // spot (when connection is active) so does not include incative time
+        // segments
         SpeedGbps effective_delivery_rate =
             ctx.delivery_rate_statistics.get_mean().value_or(SpeedGbps(0));
-        std::optional<TimeNs> cct =
-            (ctx.first_send_data_time.has_value() &&
-                     ctx.last_data_delivery_time.has_value()
-                 ? std::optional(ctx.last_data_delivery_time.value() -
-                                 ctx.first_send_data_time.value())
-                 : std::nullopt);
+        TimeNs cct = ctx.first_send_data_time.has_value() &&
+                             ctx.last_data_delivery_time.has_value()
+                         ? ctx.last_data_delivery_time.value() -
+                               ctx.first_send_data_time.value()
+                         : TimeNs(0);
         SpeedGbps delivery_rate =
-            (cct.has_value() ? SpeedGbps(ctx.total_data_delivered / cct.value())
-                             : SpeedGbps(0.0));
+            (cct != TimeNs(0) ? SpeedGbps(ctx.total_data_delivered / cct)
+                              : SpeedGbps(0.0));
         out << id;
         out << ", " << ctx.total_data_added;
         out << ", " << ctx.total_data_delivered;
         out << ", " << effective_delivery_rate;
         out << ", " << delivery_rate;
-        out << ", " << cct.value_or(TimeNs(0.0));
+        out << ", " << cct;
         out << '\n';
     }
 }
