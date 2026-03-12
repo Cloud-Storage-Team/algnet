60b5cff00d458896406a84140ec73f8973ad6455
Pavel Ralnikov <ralnikovp@gmail.com>
2026-03-12 15:03:49 +0300

    One more refactoring

--- source/parser/config_reader/config_node_with_preset_impl.hpp
+++ source/parser/config_reader/config_node_with_preset_impl.hpp
@@ -2,30 +2,30 @@
 
 #include "./config_node.hpp"
 
-namespace sim{
+namespace sim {
 
 class ConfigNodeWithPresetExpected;
 
 // Config node that supports preset inheritance.
-class ConfigNodeWithPreset{
+class ConfigNodeWithPreset {
 public:
-
     explicit ConfigNodeWithPreset(
         ConfigNode a_node,
         std::optional<ConfigNode> a_presets_node = std::nullopt,
-        std::optional<ConfigNode> a_preset = std::nullopt
-    );
+        std::optional<ConfigNode> a_preset = std::nullopt);
 
     ConfigNodeWithPresetExpected operator[](std::string_view key) const;
 
-    friend std::ostream& operator<<(std::ostream& out, const ConfigNodeWithPreset& node);
+    friend std::ostream& operator<<(std::ostream& out,
+                                    const ConfigNodeWithPreset& node);
 
     // Returns the base config node.
     const ConfigNode& get_node() const noexcept;
 
     [[nodiscard]] const std::optional<std::string>& get_name() const;
 
-    // Converts m_node to type T. Probably this method will return message about error
+    // Converts m_node to type T. Probably this method will return message about
+    // error
     template <typename T>
     [[nodiscard]] utils::StrExpected<T> as() const noexcept {
         return m_node.as<T>();
@@ -43,13 +43,13 @@ public:
     std::runtime_error create_parsing_error(std::string_view error) const;
 
 private:
-
     // m_node contains information about config node and probably preset name
     // m_preset - preset node which that is used to supplement fields of m_node
-    // m_presets_node - node it which preset node should be searched (id it needs)
+    // m_presets_node - node it which preset node should be searched (id it
+    // needs)
     const ConfigNode m_node;
     const std::optional<ConfigNode> m_presets_node;
     mutable std::optional<ConfigNode> m_preset;
 };
 
-} // namespace sim 
+}  // namespace sim
