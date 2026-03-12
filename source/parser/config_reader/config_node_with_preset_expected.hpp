8f2ddd600b2f419e415fb4cb4f544771d02f4d96
Pavel Ralnikov <ralnikovp@gmail.com>
2026-03-12 15:06:40 +0300

    ConfigNodeWithPresetExpected refactoring

--- source/parser/config_reader/config_node_with_preset_expected.hpp
+++ source/parser/config_reader/config_node_with_preset_expected.hpp
@@ -4,19 +4,20 @@
 
 #include "utils/str_expected.hpp"
 
-namespace sim{
+namespace sim {
 
 class ConfigNodeWithPreset;
 
-class ConfigNodeWithPresetExpected: public utils::StrExpected<ConfigNodeWithPreset>{
+class ConfigNodeWithPresetExpected
+    : public utils::StrExpected<ConfigNodeWithPreset> {
 public:
     using utils::StrExpected<ConfigNodeWithPreset>::StrExpected;
-    
+
     ConfigNodeWithPresetExpected(ConfigNodeWithPreset a_node);
 
-    template<typename T>
-    [[nodiscard]] utils::StrExpected<T> as() const noexcept{
-        if (!this->has_value()){
+    template <typename T>
+    [[nodiscard]] utils::StrExpected<T> as() const noexcept {
+        if (!this->has_value()) {
             return std::unexpected(this->error());
         }
         return utils::StrExpected<T>(this->value().template as<T>());
@@ -24,7 +25,8 @@ public:
 
     [[nodiscard]] utils::StrExpected<std::string> get_name() const;
 
-    [[nodiscard]] ConfigNodeWithPresetExpected operator[](std::string_view key) const;
+    [[nodiscard]] ConfigNodeWithPresetExpected operator[](
+        std::string_view key) const;
 };
 
-} // namespace sim
+}  // namespace sim
