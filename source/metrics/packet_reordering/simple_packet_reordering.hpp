bc5cf99cd0b095a76066aa76d67b2370d36ec022
Pavel Ralnikov <ralnikovp@gmail.com>
2026-03-18 12:54:19 +0300

    Rework SimlePacketReordering

--- source/metrics/packet_reordering/simple_packet_reordering.hpp
+++ source/metrics/packet_reordering/simple_packet_reordering.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include "i_packet_reordering.hpp"
-#include "utils/ordered_set.hpp"
 #include "utils/statistics.hpp"
 
 namespace sim {
+
+// Calculates count of packets that arrived not in right order
 class SimplePacketReordering : public IPacketReordering {
 public:
-    explicit SimplePacketReordering(std::size_t a_max_packets = 512);
-    ~SimplePacketReordering() = default;
+    SimplePacketReordering();
 
     void add_record(PacketNum packet_num) final;
     PacketReordering value() const final;
@@ -15,8 +15,7 @@ public:
 private:
     // maximal count of packets in m_packet_num_set
     // When m_packet_num_set.size() growth up it, the smallest element deletes
-    std::size_t m_max_packets;
-    uint64_t m_inversions_count;
-    utils::ordered_set<PacketNum> m_packet_num_set;
+    std::size_t m_next_packet_num;
+    PacketReordering m_reordering;
 };
 }  // namespace sim
