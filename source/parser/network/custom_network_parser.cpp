707d5ce64b3877cd5ba04a9e0111a3aa2571fb43
Pavel Ralnikov <ralnikovp@gmail.com>
2026-04-09 16:25:46 +0300

    Refactoring

---
+++ source/parser/network/custom_network_parser.cpp
@@ -0,0 +1,31 @@
+#include "custom_network_parser.hpp"
+
+#include "connection/connection_parser.hpp"
+#include "parser/topology/topology_parser.hpp"
+
+namespace sim {
+
+Network parse_custom_network(const Topology& topology, const ConfigNode& node) {
+    const ConfigNode& connections_node = node["connections"].value_or_throw();
+
+    NetworkContext ctx(topology, utils::IdTable<IConnection>{});
+
+    std::optional<ConfigNode> presets_node = node["presets"].to_optional();
+
+    for (const auto& connection_node : connections_node) {
+        ConfigNodeWithPreset conn_node_with_preset(connection_node,
+                                                   presets_node);
+
+        std::shared_ptr<IConnection> connection = parse_i_connection(
+            conn_node_with_preset, ctx.topology.get_context().hosts_table);
+        Id conn_id = connection->get_id();
+        if (!ctx.connections_table.emplace(conn_id, connection).second) {
+            throw connections_node.create_parsing_error(
+                fmt::format("Two connection with same name: {}", conn_id));
+        }
+    }
+
+    return Network{std::move(ctx)};
+}
+
+}  // namespace sim
