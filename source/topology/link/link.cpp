966c96f947829fdbaf83040c924fae960471d566
Pavel Ralnikov <ralnikovp@gmail.com>
2026-04-16 13:53:31 +0300

    Add -w run flag; improve README

--- source/topology/link/link.cpp
+++ source/topology/link/link.cpp
@@ -191,9 +191,9 @@ void Link::write_inner_metrics(std::filesystem::path output_dir) const {
     }
     write_thoughput_to_csv(out);
     out << '\n';
-    m_to_ingress.write_metrics_to_csv(out);
-    out << '\n';
     m_from_egress.write_metrics_to_csv(out);
+    out << '\n';
+    m_to_ingress.write_metrics_to_csv(out);
 }
 
 void Link::write_thoughput_to_csv(std::ofstream& out) const {
