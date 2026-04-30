53d2e30489498f41249940250aa267f8064308ef
Павел Ральников <112890673+PaulRalnikov@users.noreply.github.com>
2026-04-30 11:16:01 +0300

    FEAT: implement the DCQCN (#548)
    closes #176

---
+++ source/network/connection/mplb/rdma/dcqcn.hpp
@@ -0,0 +1,111 @@
+#pragma once
+#include <optional>
+
+#include "types.hpp"
+
+namespace sim {
+
+// DQCCN congestion control realization
+// Based on NVIDIA documentation:
+// https://enterprise-support.nvidia.com/s/article/DCQCN-CC-algorithm
+// https://enterprise-support.nvidia.com/s/article/dcqcn-parameters
+
+struct ParamsDQCCN {
+    // -------------Rate increment-------------
+
+    // The time period between rate increase events.
+    TimeUs rpg_time_reset = TimeUs(300);
+
+    // 	The sent bytes counter between rate increase events.
+    SizeByte rpg_byte_reset = SizeByte(64 * 32767ul);
+
+    // The threshold of rate increase events for moving to next rate increase
+    // phase.
+    std::size_t rpg_threshold = 1;
+
+    // The rate increase value in the Additive Increase phase.
+    SpeedMbps rpg_ai_rate = SpeedMbps(5);
+
+    // The rate increase value in the Hyper Increase phase.
+    SpeedMbps rpg_hai_rate = SpeedMbps(50);
+
+    // -------------Alpha update-------------
+
+    // This parameter sets the initial value of alpha that should be used when
+    // receiving the first CNP for a flow.
+    int initial_alpha_value = 1023;
+
+    // Controls aggressiveness of alpha's updates
+    // The lower G is, the more aggressive are the changes.
+    int dce_tcp_g = 1019;
+
+    // The Time period between alpha updates.
+    TimeUs dce_tcp_rtt = TimeUs(1);
+
+    // -------------Rate decrement-------------
+
+    // The time period between rate reductions.
+    TimeUs rate_reduce_monitor_period = TimeUs(4);
+
+    // Rates (current, target) on first CNP (0 – 85% of line rate).
+    SpeedMbps rate_to_set_on_first_cnp = SpeedMbps(0);
+
+    // If true, every rate decreases. The target rate is updated to the current
+    // rate.
+    // Otherwise, the target rate is updated to the current rate only on the
+    // first decrement after the increment event.
+    bool clamp_tgt_rate = false;
+
+    // The coefficient between alpha and the rate reduction factor.
+    // Log2 of value in fixed point with 10 in the fraction part
+    int rpg_gd = 11;
+
+    // Minimal rate limit of the QP.
+    SpeedMbps rpg_min_rate = SpeedMbps(1);
+    // Maximal rate limit of the QP.
+    double rpg_min_dec_fac = 0.5;
+};
+
+class DCQCN {
+public:
+    explicit DCQCN(const ParamsDQCCN& a_params);
+
+    // enqueue initial events
+    void start();
+
+    // stop creating new events
+    void stop();
+
+    // Calls when sender got congestion notification
+    void on_cnp();
+
+    // Calls when sender got asknowledge of receiving data_size data
+    void on_data_delivery(SizeByte data_size);
+
+    SpeedMbps get_rate() const;
+
+private:
+    void on_rate_reduce_monitor_period();
+    void on_alpha_timer();
+
+    void on_rate_increase_timer(std::optional<TimeNs> last_elapced_cnp);
+    void on_rate_increase_event();
+
+    ParamsDQCCN m_params;
+    SpeedMbps m_current_rate;
+    SpeedMbps m_target_rate;
+    bool m_dec_target_rate = false;
+
+    // Size & time counters (T & BC on Increment scheme part of NVIDIA docs)
+    SizeByte m_bytes_from_last_byte_reset = SizeByte(0ul);
+    std::uint32_t m_time_counter = 0;
+    std::uint32_t m_byte_counter = 0;
+
+    // Last time CNP was got
+    std::optional<TimeNs> m_last_cnp = std::nullopt;
+
+    int m_alpha;
+    bool m_stop_request = false;
+};
+
+}  // namespace sim
---
+++ test/connection/rdma/dcqcn_test.cpp
@@ -0,0 +1,111 @@
+#include "network/connection/mplb/rdma/dcqcn.hpp"
+
+#include <gtest/gtest.h>
+
+#include "scheduler/scheduler.hpp"
+#include "utils/defer.hpp"
+
+namespace sim {
+namespace test2 {
+
+struct Fixture : public ::testing::Test {
+    Fixture() : params() {
+        // задать удобные параметры для детерминированного тестирования
+        params.rpg_time_reset = TimeUs(100);
+        params.rpg_byte_reset = SizeByte(1000ul);
+        params.rpg_threshold = 1;
+        params.rpg_ai_rate = SpeedMbps(10);
+        params.rpg_hai_rate = SpeedMbps(100);
+        params.dce_tcp_rtt = TimeUs(10);
+        params.rate_reduce_monitor_period = TimeUs(20);
+        params.initial_alpha_value = 1023;
+        params.dce_tcp_g = 1019;
+        params.rpg_min_rate = SpeedMbps(100);
+        params.rpg_min_dec_fac = 0.5;
+    }
+
+    void TearDown() override { Scheduler::get_instance().clear(); };
+    void SetUp() override { Scheduler::get_instance().clear(); }
+
+    ParamsDQCCN params;
+};
+
+// Helper: устанавливаем начальный момент времени
+TimeNs now_ns(int us) { return TimeNs(us * 1000); }
+
+TEST_F(Fixture, AdditiveIncreaseByTimer) {
+    DCQCN cc(params);
+    cc.start();
+    utils::Defer defer([&cc]() { cc.stop(); });
+
+    SpeedMbps before = cc.get_rate();
+
+    // one additive increase should be triggered
+    Scheduler::get_instance().tick_to(params.rpg_time_reset);
+
+    SpeedMbps new_rate = cc.get_rate();
+    EXPECT_GT(new_rate, before);
+}
+
+TEST_F(Fixture, AdditiveIncreaseByBytes) {
+    DCQCN cc(params);
+    cc.start();
+    utils::Defer defer([&cc]() { cc.stop(); });
+
+    SpeedMbps before = cc.get_rate();
+
+    cc.on_data_delivery(params.rpg_byte_reset);
+    SpeedMbps after = cc.get_rate();
+    EXPECT_GT(after, before);
+}
+
+TEST_F(Fixture, AdditiveAndHyperIncrease) {
+    DCQCN cc(params);
+    cc.start();
+    utils::Defer defer([&cc]() { cc.stop(); });
+
+    SpeedMbps rate_start = cc.get_rate();
+
+    // one additive increase
+    cc.on_data_delivery(params.rpg_byte_reset);
+
+    SpeedGbps rate_ai = cc.get_rate();
+    EXPECT_GT(rate_ai, rate_start);
+
+    // two more additive increase
+    Scheduler::get_instance().tick_to(params.rpg_time_reset * 2);
+
+    SpeedMbps rate_before_hai = cc.get_rate();
+
+    // one hyper increase
+    cc.on_data_delivery(params.rpg_byte_reset);
+
+    SpeedMbps rate_hai = cc.get_rate();
+
+    EXPECT_GT(rate_hai, rate_before_hai);
+
+    static constexpr double thresh = 2.0;
+
+    EXPECT_GT(rate_hai - rate_before_hai, thresh * (rate_ai - rate_start));
+}
+
+TEST_F(Fixture, RateDecreaseOnCnp) {
+    DCQCN cc(params);
+    cc.start();
+    utils::Defer defer([&cc]() { cc.stop(); });
+
+    // increase speed
+    cc.on_data_delivery(params.rpg_byte_reset);
+    cc.on_data_delivery(params.rpg_byte_reset);
+    SpeedMbps up = cc.get_rate();
+
+    cc.on_cnp();
+
+    Scheduler::get_instance().tick_to(params.rate_reduce_monitor_period);
+    SpeedMbps down = cc.get_rate();
+
+    EXPECT_LT(down, up);
+}
+
+}  // namespace test2
+}  // namespace sim
